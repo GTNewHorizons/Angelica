99a40ae90b2ffdca7a25b4f56b8c7d2cf55c1b7d
Raven Szewczyk <git@eigenraven.me>
2022-08-22 20:43:48 +0100

    Move the namer classes out of todo

---
+++ src/main/java/com/gtnewhorizons/angelica/transform/NamerMcf.java
@@ -0,0 +1,9 @@
+package com.gtnewhorizons.angelica.transform;
+
+public class NamerMcf extends NamerMcp {
+
+    public void setNames() {
+        setNamesSrg();
+        rename("../build/unpacked/conf/");
+    }
+}
