d7d4ecc11bd6c3390b56dfaa0bc9ef949fec9b9f
glowredman <35727266+glowredman@users.noreply.github.com>
2023-08-22 21:48:29 +0200

    Rewrite ACTRenderSpider as Mixins

--- src/main/java/com/gtnewhorizons/angelica/transform/AClassTransformer.java
+++ src/main/java/com/gtnewhorizons/angelica/transform/AClassTransformer.java
@@ -26,9 +26,6 @@ public class AClassTransformer implements IClassTransformer {
         put(Names.entityRenderer_, new ACTEntityRenderer());
         put(Names.renderManager_, new ACTRenderManager());
         put(Names.rendererLivingE_, new ACTRendererLivingEntity());
-        put(Names.renderDragon_, new ACTRenderSpider());
-        put(Names.renderEnderman_, new ACTRenderSpider());
-        put(Names.renderSpider_, new ACTRenderSpider());
         put(Names.textureDownload_, new ACTTextureDownload());
         put(Names.abstractTexture_, new ACTTextureAbstract());
         put(Names.iTextureObject_, new ACTTextureObject());
