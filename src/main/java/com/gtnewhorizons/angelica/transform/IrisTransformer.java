70e573d2776f9b5f18ff225ab937ba7c479280e4
Jason Mitchell <mitchej@gmail.com>
2023-10-30 15:19:38 -0700

    Loosen TransformerExclusion Also take a stab at blacklisting via the transformer itself...

--- src/main/java/com/gtnewhorizons/angelica/transform/IrisTransformer.java
+++ src/main/java/com/gtnewhorizons/angelica/transform/IrisTransformer.java
@@ -9,12 +9,14 @@ import org.spongepowered.asm.lib.ClassWriter;
 import org.spongepowered.asm.lib.MethodVisitor;
 import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.lib.commons.Method;
-import org.spongepowered.asm.lib.commons.Remapper;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class IrisTransformer extends Remapper implements IClassTransformer {
+public class IrisTransformer implements IClassTransformer {
     /*
      * Redirects a subset of GL11 calls to GLStateManager
      *  NOTE: Still need to verify compatibility with Mixins and Lwjgl3ify
@@ -26,14 +28,22 @@ public class IrisTransformer extends Remapper implements IClassTransformer {
 
     private static final Set<String> EnabledRedirects = Sets.newHashSet("glBindTexture", "glTexImage2D", "glDeleteTextures");
 
+    private static final List<String> TransformerExclusions = new ArrayList<>(Arrays.asList(
+        GLStateTrackerClass, "org.lwjgl", "com.gtnewhorizons.angelica.transform", "me.eigenraven.lwjgl3ify")
+    );
     public static int remaps = 0, calls = 0;
 
     @Override
     public byte[] transform(final String className, String transformedName, byte[] basicClass) {
         if (basicClass == null) return null;
 
-        // Ignore the state tracker itself
-        if (className.equals(GLStateTrackerClass)) return basicClass;
+
+        // Ignore classes that are excluded from transformation - Doesn't seem to fully work without the TransformerExclusions
+        for (String exclusion : TransformerExclusions) {
+            if (className.startsWith(exclusion)) {
+                return basicClass;
+            }
+        }
 
         ClassReader reader = new ClassReader(basicClass);
         Set<Method> eligibleMethods = findEligibleMethods(reader);
@@ -42,14 +52,14 @@ public class IrisTransformer extends Remapper implements IClassTransformer {
         }
 
         ClassWriter writer = new ClassWriter(reader, 0);
-        reader.accept(new ClassVisitor(Opcodes.ASM5, writer) {
+        reader.accept(new ClassVisitor(Opcodes.ASM9, writer) {
             @Override
             public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
                 MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
                 if (!eligibleMethods.contains(new Method(name, desc))) {
                     return mv;
                 }
-                return new MethodVisitor(Opcodes.ASM5, mv) {
+                return new MethodVisitor(Opcodes.ASM9, mv) {
                     @Override
                     public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
                         if (owner.equals(GL11) && EnabledRedirects.contains(name)) {
@@ -68,10 +78,10 @@ public class IrisTransformer extends Remapper implements IClassTransformer {
 
     private Set<Method> findEligibleMethods(ClassReader reader) {
         Set<Method> eligibleMethods = new HashSet<>();
-        reader.accept(new ClassVisitor(Opcodes.ASM5) {
+        reader.accept(new ClassVisitor(Opcodes.ASM9) {
             @Override
             public MethodVisitor visitMethod(int access, String methodName, String methodDesc, String signature, String[] exceptions) {
-                return new MethodVisitor(Opcodes.ASM5) {
+                return new MethodVisitor(Opcodes.ASM9) {
                     @Override
                     public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
                         if (owner.equals(GL11) && EnabledRedirects.contains(name)) {
