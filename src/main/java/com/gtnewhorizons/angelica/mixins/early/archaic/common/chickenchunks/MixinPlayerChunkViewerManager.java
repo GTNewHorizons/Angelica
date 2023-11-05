4ee5adee4e8facf52e8c07a08a8bdae0f020098e
glowredman <35727266+glowredman@users.noreply.github.com>
2023-11-05 16:07:39 +0100

    Use `@Share` / `@Local`

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/archaic/common/chickenchunks/MixinPlayerChunkViewerManager.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/archaic/common/chickenchunks/MixinPlayerChunkViewerManager.java
@@ -6,34 +6,28 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 @Mixin(PlayerChunkViewerManager.class)
 public class MixinPlayerChunkViewerManager {
     @Shadow(remap = false) public LinkedList<PlayerChunkViewerManager.TicketChange> ticketChanges;
 
-    private ArrayList<PlayerChunkViewerManager.TicketChange> oldTicketChanges;
-
-    @Redirect(method = "update", at = @At(value = "INVOKE", target = "Ljava/util/LinkedList;iterator()Ljava/util/Iterator;"), remap = false)
-    private Iterator<?> getSafeIterator(LinkedList<?> list) {
-        if(list == this.ticketChanges) {
-            oldTicketChanges = new ArrayList<>((LinkedList<PlayerChunkViewerManager.TicketChange>)list);
-            return oldTicketChanges.iterator();
-        } else {
-            return list.iterator();
-        }
+    @Redirect(method = "update", at = @At(ordinal = 7, value = "INVOKE", target = "Ljava/util/LinkedList;iterator()Ljava/util/Iterator;"), remap = false)
+    private Iterator<?> getSafeIterator(LinkedList<PlayerChunkViewerManager.TicketChange> list, @Share("oldTicketChanges") LocalRef<List<PlayerChunkViewerManager.TicketChange>> oldTicketChanges) {
+        oldTicketChanges.set(new ArrayList<>(list));
+        return oldTicketChanges.get().iterator();
     }
 
-    @Redirect(method = "update", at = @At(value = "INVOKE", target = "Ljava/util/LinkedList;clear()V"), remap = false)
-    private void clearListSafely(LinkedList<?> list) {
-        if(list == this.ticketChanges) {
-            this.ticketChanges.removeAll(oldTicketChanges);
-            oldTicketChanges = null;
-        } else {
-            list.clear();
-        }
+    @Redirect(method = "update", at = @At(ordinal = 4, value = "INVOKE", target = "Ljava/util/LinkedList;clear()V"), remap = false)
+    private void clearListSafely(LinkedList<PlayerChunkViewerManager.TicketChange> list, @Share("oldTicketChanges") LocalRef<List<PlayerChunkViewerManager.TicketChange>> oldTicketChanges) {
+        this.ticketChanges.removeAll(oldTicketChanges.get());
+        oldTicketChanges.set(null);
     }
 
 }
