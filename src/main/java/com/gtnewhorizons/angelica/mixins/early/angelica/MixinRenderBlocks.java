7f97ee7126f4a95ea7bd98f44db02d245ee28ffa
Jason Mitchell <mitchej@gmail.com>
2023-11-17 09:43:39 -0800

    One inject with multiple methods instead of two injects

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinRenderBlocks.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinRenderBlocks.java
@@ -3,7 +3,6 @@ package com.gtnewhorizons.angelica.mixins.early.angelica;
 import com.gtnewhorizons.angelica.mixins.interfaces.IHasTessellator;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,11 +18,7 @@ public class MixinRenderBlocks implements IHasTessellator {
         return sodium$tessellator;
     }
 
-    @Inject(method = "<init>(Lnet/minecraft/world/IBlockAccess;)V", at = @At("TAIL"))
-    private void initTessellator(IBlockAccess world, CallbackInfo ci) {
-        sodium$tessellator = new Tessellator();
-    }
-    @Inject(method = "<init>()V", at = @At("TAIL"))
+    @Inject(method = {"<init>()V","<init>(Lnet/minecraft/world/IBlockAccess;)V"}, at = @At("TAIL"))
     private void initTessellator(CallbackInfo ci) {
         sodium$tessellator = new Tessellator();
     }
