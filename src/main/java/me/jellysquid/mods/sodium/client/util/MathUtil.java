d54f9df6e5f140b13593e72c668d79b4402e0147
rjnasers <rjnasers@iastate.edu>
2023-11-22 00:39:33 -0600

    Removing clamp implementation because I missed the one in mathhelper

--- src/main/java/me/jellysquid/mods/sodium/client/util/MathUtil.java
+++ src/main/java/me/jellysquid/mods/sodium/client/util/MathUtil.java
@@ -7,20 +7,4 @@ public class MathUtil {
     public static boolean isPowerOfTwo(int n) {
         return ((n & (n - 1)) == 0);
     }
-
-    /**
-     * @param value The value to be clamped
-     * @param min minimum that the value will be clamped to
-     * @param max maximum that the value will be clamped to
-     * @return If the value is within the rand (min -> max) then it returns value, otherwise returns
-     * minimum if value is smaller than minimum and maximum if value is larger than maximum.
-     */
-    public static double clamp(double value, double min, double max) {
-        if (value < min) {
-            return min;
-        } else {
-            return value > max ? max : value;
-        }
-    }
-
 }
