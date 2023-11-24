3fbaac3914940fcada64dc8903d41bbedf8d5296
makamys <makamys@outlook.com>
2023-11-24 08:14:46 +0100

    Make CompatMemoryUtil#memReallocDirect preserve buffer position
    LWJGL3's MemoryUtil#memRealloc does this, see its javadoc.
    
    Fixes screen flashing and this error getting printed when indirect buffer grows:
    GL_INVALID_OPERATION error generated. Bound draw indirect buffer is not large enough.

--- src/main/java/com/gtnewhorizons/angelica/compat/lwjgl/CompatMemoryUtil.java
+++ src/main/java/com/gtnewhorizons/angelica/compat/lwjgl/CompatMemoryUtil.java
@@ -7,9 +7,10 @@ import java.nio.ByteBuffer;
 public class CompatMemoryUtil {
     public static ByteBuffer memReallocDirect(ByteBuffer old, int capacity) {
         ByteBuffer newBuf = BufferUtils.createByteBuffer(capacity);
+        int oldPos = old.position();
         old.rewind();
         newBuf.put(old);
-        newBuf.position(0);
+        newBuf.position(Math.min(capacity, oldPos));
         return newBuf;
     }
 }
