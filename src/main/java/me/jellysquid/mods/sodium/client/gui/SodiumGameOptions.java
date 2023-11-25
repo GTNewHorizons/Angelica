783bd5613d3e568b2f79abba212e1ac50d4e2aba
Asek3 <64613894+Asek3@users.noreply.github.com>
2023-11-25 19:54:58 +0200

    Fixed Weather option

--- src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
+++ src/main/java/me/jellysquid/mods/sodium/client/gui/SodiumGameOptions.java
@@ -5,6 +5,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import me.jellysquid.mods.sodium.client.SodiumClientMod;
+import net.minecraft.client.Minecraft;
 
 
 import java.io.FileReader;
@@ -111,6 +112,10 @@ public class SodiumGameOptions {
 
         GraphicsQuality(String name) {
             this.name = name;
+        }
+
+        public boolean isFancy() {
+            return this == FANCY || (this == DEFAULT && Minecraft.getMinecraft().gameSettings.fancyGraphics);
         }
     }
 
