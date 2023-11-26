9c64c9db6513828d3dd21b47ff56b6e8763201a2
Jason Mitchell <mitchej@gmail.com>
2023-11-25 22:47:34 -0800

    Add Iris Sodium Compat impl

---
+++ src/main/java/net/coderbot/iris/sodium/vertex_format/IrisChunkMeshAttributes.java
@@ -0,0 +1,14 @@
+package net.coderbot.iris.sodium.vertex_format;
+
+import me.jellysquid.mods.sodium.client.render.chunk.format.ChunkMeshAttribute;
+
+/**
+ * Initialized by {@link net.coderbot.iris.compat.sodium.mixin.vertex_format.MixinChunkMeshAttribute}
+ */
+public class IrisChunkMeshAttributes {
+	public static ChunkMeshAttribute NORMAL;
+	public static ChunkMeshAttribute TANGENT;
+	public static ChunkMeshAttribute MID_TEX_COORD;
+	public static ChunkMeshAttribute BLOCK_ID;
+	public static ChunkMeshAttribute MID_BLOCK;
+}
