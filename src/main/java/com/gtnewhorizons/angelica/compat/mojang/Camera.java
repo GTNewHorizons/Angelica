33052562463f351ee5a5d87bd2c0e872e4c16eff
makamys <makamys@outlook.com>
2023-11-26 22:05:17 +0100

    Fix face culling not taking third person camera offset into account

--- src/main/java/com/gtnewhorizons/angelica/compat/mojang/Camera.java
+++ src/main/java/com/gtnewhorizons/angelica/compat/mojang/Camera.java
@@ -3,7 +3,12 @@ package com.gtnewhorizons.angelica.compat.mojang;
 import lombok.Getter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityLivingBase;
+
+import org.joml.Matrix4f;
 import org.joml.Vector3d;
+import org.joml.Vector4f;
+
+import com.gtnewhorizons.angelica.rendering.RenderingState;
 
 @Getter
 public class Camera {
@@ -15,10 +20,15 @@ public class Camera {
     boolean thirdPerson;
 
     public Camera(EntityLivingBase entity, float partialTicks) {
-        final double camX = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * partialTicks;
-        final double camY = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * partialTicks;
-        final double camZ = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * partialTicks;
+        Vector4f offset = new Vector4f(); // third person offset
+        final Matrix4f inverseModelView = new Matrix4f(RenderingState.INSTANCE.getModelViewMatrix()).invert();
+        inverseModelView.transform(offset);
+        
+        final double camX = entity.lastTickPosX + (entity.posX - entity.lastTickPosX) * partialTicks + offset.x;
+        final double camY = entity.lastTickPosY + (entity.posY - entity.lastTickPosY) * partialTicks + offset.y;
+        final double camZ = entity.lastTickPosZ + (entity.posZ - entity.lastTickPosZ) * partialTicks + offset.z;
         this.entity = entity;
+        
         pos.set(camX, camY, camZ);
         blockPos.set((int)entity.posX, (int)entity.posY, (int)entity.posZ);
         pitch = entity.cameraPitch;
