0a25f6dd677cc30de455cca6fc85ea1203ad3271
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2023-11-27 01:28:20 +0100

    use ClassConstantPoolParser in TessellatorTransformer to speed up search for Tessellator references

--- src/main/java/com/gtnewhorizons/angelica/transform/TessellatorTransformer.java
+++ src/main/java/com/gtnewhorizons/angelica/transform/TessellatorTransformer.java
@@ -1,13 +1,18 @@
 package com.gtnewhorizons.angelica.transform;
 
 import net.minecraft.launchwrapper.IClassTransformer;
-import org.spongepowered.asm.lib.*;
+import org.spongepowered.asm.lib.ClassReader;
+import org.spongepowered.asm.lib.ClassWriter;
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.lib.tree.*;
 
 /**
- * Redirects all Tessellator.instance accesses to go through our TessellatorManager.
+ * Redirects all Tessellator.instance field accesses to go through our TessellatorManager.
  */
 public class TessellatorTransformer implements IClassTransformer {
+
+    private static final ClassConstantPoolParser parser = new ClassConstantPoolParser("net/minecraft/client/renderer/Tessellator");
+
     @Override
     public byte[] transform(final String className, String transformedName, byte[] basicClass) {
         if (basicClass == null) return null;
@@ -19,29 +24,33 @@ public class TessellatorTransformer implements IClassTransformer {
             }
         }
 
+        if (!parser.find(basicClass)) {
+            return basicClass;
+        }
+
         final ClassReader reader = new ClassReader(basicClass);
         ClassNode node = new ClassNode();
         reader.accept(node, Opcodes.ASM9);
         boolean changed = false;
-        for(MethodNode m : node.methods) {
-            for(AbstractInsnNode insn : m.instructions) {
-                if(insn.getOpcode() == Opcodes.GETSTATIC) {
-                    FieldInsnNode fNode = (FieldInsnNode)insn;
-                    if((fNode.name.equals("field_78398_a") || fNode.name.equals("instance")) && fNode.owner.equals("net/minecraft/client/renderer/Tessellator")) {
+        for (MethodNode m : node.methods) {
+            for (AbstractInsnNode insn : m.instructions) {
+                if (insn.getOpcode() == Opcodes.GETSTATIC) {
+                    FieldInsnNode fNode = (FieldInsnNode) insn;
+                    if ((fNode.name.equals("field_78398_a") || fNode.name.equals("instance")) && fNode.owner.equals("net/minecraft/client/renderer/Tessellator")) {
                         MethodInsnNode getNode = new MethodInsnNode(Opcodes.INVOKESTATIC, "com/gtnewhorizons/angelica/glsm/TessellatorManager", "get", "()Lnet/minecraft/client/renderer/Tessellator;", false);
                         m.instructions.set(fNode, getNode);
                         changed = true;
-                        break;
                     }
                 }
             }
         }
 
-        if(!changed)
-            return basicClass;
+        if (changed) {
+            ClassWriter writer = new ClassWriter(reader, 0);
+            node.accept(writer);
+            return writer.toByteArray();
+        }
+        return basicClass;
 
-        ClassWriter writer = new ClassWriter(reader, 0);
-        node.accept(writer);
-        return writer.toByteArray();
     }
 }
