b31680909b922fe34ca4ca97dda20b8d75f76e66
Jason Mitchell <mitchej@gmail.com>
2023-12-03 10:16:48 -0800

    Add another onTexImage2D overload. * Unclear if needed, but can remove it later if it's not

--- src/main/java/net/coderbot/iris/texture/TextureInfoCache.java
+++ src/main/java/net/coderbot/iris/texture/TextureInfoCache.java
@@ -25,8 +25,16 @@ public class TextureInfoCache {
 		return info;
 	}
 
-	public void onTexImage2D(int target, int level, int internalformat, int width, int height, int border,
-							 int format, int type, @Nullable IntBuffer pixels) {
+	public void onTexImage2D(int target, int level, int internalformat, int width, int height, int border, int format, int type, @Nullable IntBuffer pixels) {
+		if (level == 0) {
+			int id = GL11.glGetInteger(GL11.GL_TEXTURE_BINDING_2D);
+			TextureInfo info = getInfo(id);
+			info.internalFormat = internalformat;
+			info.width = width;
+			info.height = height;
+		}
+	}
+    public void onTexImage2D(int target, int level, int internalformat, int width, int height, int border, int format, int type, long pixels_buffer_offset) {
 		if (level == 0) {
 			int id = GL11.glGetInteger(GL11.GL_TEXTURE_BINDING_2D);
 			TextureInfo info = getInfo(id);
