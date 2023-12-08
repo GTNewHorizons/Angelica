9cf89eb8cb1881e4a69f4decfe1e55895c4a1008
Caedis <Caedis@users.noreply.github.com>
2023-12-08 12:59:27 -0600

    clean up

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinExtendedBlockStorage.java
+++
@@ -1,9 +0,0 @@
-package com.gtnewhorizons.angelica.mixins.early.angelica;
-
-import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(ExtendedBlockStorage.class)
-public class MixinExtendedBlockStorage {
-
-}
