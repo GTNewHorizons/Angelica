95cc3ee9574161e094626043ae365d3ca6c6c76f
rjnasers <rjnasers@iastate.edu>
2023-12-09 23:18:58 -0600

    Adding some error handling, I really don't know exactly what I should be doing for errors or how to know when to throw one

--- src/main/java/com/gtnewhorizons/angelica/transform/RenderItemTransformer.java
+++ src/main/java/com/gtnewhorizons/angelica/transform/RenderItemTransformer.java
@@ -1,14 +1,8 @@
 package com.gtnewhorizons.angelica.transform;
 
-import net.minecraft.client.renderer.OpenGlHelper;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.item.Item;
 import net.minecraft.launchwrapper.IClassTransformer;
-import org.lwjgl.opengl.GL11;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
-import org.objectweb.asm.FieldVisitor;
 import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
@@ -16,6 +10,7 @@ import org.objectweb.asm.tree.IntInsnNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
+import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
 import static org.objectweb.asm.Opcodes.INVOKESTATIC;
 import static org.objectweb.asm.Opcodes.GETSTATIC;
 
@@ -46,6 +41,7 @@ public class RenderItemTransformer implements IClassTransformer {
         }
         catch (Exception e)
         {
+            System.out.println("FATAL ERROR in RenderItemTransformer");
             e.printStackTrace();
         }
         return classBeingTranformed;
@@ -70,6 +66,7 @@ public class RenderItemTransformer implements IClassTransformer {
                         if (!tesselatorFound && ((FieldInsnNode) instruction).desc.equals("Lnet/minecraft/client/renderer/Tessellator;")) {
                             targetNode = instruction;
                             removeTesselatorAndCleanupCallsBlock(targetNode, method);
+                            tesselatorFound = true;
                             break;
                         }
                     }
@@ -81,6 +78,7 @@ public class RenderItemTransformer implements IClassTransformer {
                             {
                                 targetNode = instruction;
                                 removeGLDisable(targetNode, method);
+                                glDisableFound = true;
                                 continue;
                             }
                         }
@@ -88,6 +86,7 @@ public class RenderItemTransformer implements IClassTransformer {
                         {
                             targetNode = instruction;
                             removeColorMask(targetNode, method);
+                            glColorMaskFound = true;
                         }
                     }
                 }
@@ -145,42 +144,76 @@ public class RenderItemTransformer implements IClassTransformer {
 
     private static void removeTesselatorAndCleanupCallsBlock(AbstractInsnNode targetNode, MethodNode method)
     {
-        if (targetNode != null)
+        try
         {
-            // 76 is for the opCodes of the tesselator, 12 is for the opcodes of the 3 GL calls after it
-            for (int i = 0; i < 76 + 12; i++)
+            if (targetNode != null)
             {
-                targetNode = targetNode.getNext();
                 method.instructions.remove(targetNode.getPrevious());
+                method.instructions.remove(targetNode.getPrevious());
+                while(!(targetNode.getNext().getNext().getNext().getOpcode() == INVOKEVIRTUAL && ((MethodInsnNode) targetNode.getNext().getNext().getNext()).name.equals("getItem")))
+                {
+                    targetNode = targetNode.getNext();
+                    method.instructions.remove(targetNode.getPrevious());
+                }
+            }
+            else
+            {
+                throw new Exception();
             }
         }
-        else
+        catch(Exception e)
         {
-            System.out.println("Could not find the tesselator to overwrite it");
+            System.out.println("Could not find the tesselator to remove");
+            e.printStackTrace();
         }
     }
 
     private static void removeGLDisable(AbstractInsnNode targetNode, MethodNode method)
     {
-        if (targetNode != null)
+        try
         {
-            for (int i = 0; i < 6; i++)
+            if (targetNode != null)
             {
-                targetNode = targetNode.getPrevious();
-                method.instructions.remove(targetNode.getNext());
+                while (!(targetNode.getOpcode() == INVOKEVIRTUAL && ((MethodInsnNode) targetNode).name.equals("bindTexture")))
+                {
+                    targetNode = targetNode.getPrevious();
+                    method.instructions.remove(targetNode.getNext());
+                }
+            }
+            else
+            {
+                throw new Exception();
             }
         }
+        catch(Exception e)
+        {
+            System.out.println("Could not find the correct GLDisable functions to remove");
+            e.printStackTrace();
+        }
+
     }
 
     private static void removeColorMask(AbstractInsnNode targetNode, MethodNode method)
     {
-        if (targetNode != null)
+        try
         {
-            for (int i = 0; i < 6; i++)
+            if (targetNode != null)
             {
-                targetNode = targetNode.getPrevious();
-                method.instructions.remove(targetNode.getNext());
+                while (!(targetNode.getOpcode() == INVOKESTATIC && ((MethodInsnNode) targetNode).name.equals("glBlendFunc")))
+                {
+                    targetNode = targetNode.getPrevious();
+                    method.instructions.remove(targetNode.getNext());
+                }
             }
+            else
+            {
+                throw new Exception();
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Could not find the correct colorMask function to remove");
+            e.printStackTrace();
         }
     }
 }
