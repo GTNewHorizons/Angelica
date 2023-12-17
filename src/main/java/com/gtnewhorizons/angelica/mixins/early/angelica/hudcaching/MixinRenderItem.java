0b748fea38ab0891621ceaca4e2be639562ea30c
Jason Mitchell <mitchej@gmail.com>
2023-12-16 16:25:07 -0800

    No star imports

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/hudcaching/MixinRenderItem.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/hudcaching/MixinRenderItem.java
@@ -2,14 +2,15 @@ package com.gtnewhorizons.angelica.mixins.early.angelica.hudcaching;
 
 import com.gtnewhorizons.angelica.mixins.dummy.DummyTessellator;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.RenderItem;
 import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL11.GL_LIGHTING;
 
 @Mixin(RenderItem.class)
 public class MixinRenderItem {
