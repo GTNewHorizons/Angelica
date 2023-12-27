f32c14f79572ac6b6db4707acaf8d794fe0b0457
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-12-27 15:02:15 -0500

    Add missing import

--- src/main/java/com/gtnewhorizons/angelica/transform/RedirectorTransformer.java
+++ src/main/java/com/gtnewhorizons/angelica/transform/RedirectorTransformer.java
@@ -26,6 +26,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
