04d591495efa1f8b9dfff86e94b6898e3d5e0322
Jason Mitchell <jason@puzzle.io>
2024-01-03 22:30:35 -0800

    star imports :-/

--- src/main/java/com/gtnewhorizons/angelica/mixins/Mixins.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/Mixins.java
@@ -7,7 +7,11 @@ import com.prupe.mcpatcher.Config;
 import com.prupe.mcpatcher.MCPatcherUtils;
 import cpw.mods.fml.relauncher.FMLLaunchHandler;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
