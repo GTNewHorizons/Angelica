d497c301c9848872bb485e722ce84526a6eb27ae
Darren Eberly <darren@eber.ly>
2024-01-06 20:47:58 -0500

    Fix for GUI lighting states (#148)
    * Fix for GUI lighting states
    
    * Lighting fix after RenderTickEvent because mods love to abuse it
    
    * Remove unnecessary glEnable
    
    * Remove unused import

---
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinMinecraft.java
@@ -0,0 +1,20 @@
+package com.gtnewhorizons.angelica.mixins.early.angelica;
+
+import net.minecraft.client.Minecraft;
+import org.lwjgl.opengl.GL11;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+
+    @Inject(
+        method = "runGameLoop",
+        at = @At(value = "INVOKE", target = "Lcpw/mods/fml/common/FMLCommonHandler;onRenderTickEnd(F)V", shift = At.Shift.AFTER)
+    )
+    private void angelica$injectLightingFixPostRenderTick(CallbackInfo ci) {
+        GL11.glEnable(GL11.GL_LIGHTING);
+    }
+}
