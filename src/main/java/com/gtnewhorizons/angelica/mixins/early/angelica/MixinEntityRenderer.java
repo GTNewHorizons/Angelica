c6079fb73ef4baf34cc620c77dc65eb6b355ef32
Darren Eberly <darren@eber.ly>
2024-01-06 20:03:45 -0500

    Remove unused import

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinEntityRenderer.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinEntityRenderer.java
@@ -4,7 +4,6 @@ import com.gtnewhorizons.angelica.rendering.RenderingState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraft.entity.EntityLivingBase;
-import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
