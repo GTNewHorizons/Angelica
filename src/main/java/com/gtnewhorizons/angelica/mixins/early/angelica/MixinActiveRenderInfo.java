59ff900a16bdf9bec269feb4b1d33a28e0a47b51
Jason Mitchell <mitchej@gmail.com>
2024-01-27 09:32:47 -0800

    More aggressive caching

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinActiveRenderInfo.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinActiveRenderInfo.java
@@ -1,14 +1,11 @@
 package com.gtnewhorizons.angelica.mixins.early.angelica;
 
-import com.gtnewhorizons.angelica.glsm.GLStateManager;
 import com.gtnewhorizons.angelica.rendering.RenderingState;
 import net.minecraft.client.renderer.ActiveRenderInfo;
-import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.nio.FloatBuffer;
@@ -21,14 +18,5 @@ public class MixinActiveRenderInfo {
     private static void angelica$onUpdateRenderInfo(CallbackInfo ci) {
         RenderingState.INSTANCE.setProjectionMatrix(projection);
         RenderingState.INSTANCE.setModelViewMatrix(modelview);
-    }
-
-    @Redirect(method = "updateRenderInfo", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL11;glGetInteger(ILjava/nio/IntBuffer;)V"), remap = false)
-    private static void angelica$glGetInteger(int pname, java.nio.IntBuffer params) {
-        if(pname == GL11.GL_VIEWPORT) {
-            GLStateManager.getViewportState().get(params);
-        } else {
-            GL11.glGetInteger(pname, params);
-        }
     }
 }
