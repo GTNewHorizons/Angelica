ea7bd4269678857aafa7ccf455b255c86409e8e7
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-02-27 06:50:15 -0500

    Batch colorizing

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/models/MixinBlockLeaves.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/models/MixinBlockLeaves.java
@@ -1,12 +1,9 @@
 package com.gtnewhorizons.angelica.mixins.early.angelica.models;
 
+import com.gtnewhorizons.angelica.api.QuadProvider;
 import com.gtnewhorizons.angelica.compat.mojang.BlockPos;
 import com.gtnewhorizons.angelica.compat.nd.Quad;
 import com.gtnewhorizons.angelica.utils.ObjectPooler;
-import java.util.List;
-import java.util.Random;
-import com.gtnewhorizons.angelica.models.CubeModel;
-import com.gtnewhorizons.angelica.api.QuadProvider;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.block.material.Material;
@@ -14,14 +11,16 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.util.ForgeDirection;
 import org.spongepowered.asm.mixin.Mixin;
 
+import java.util.List;
+import java.util.Random;
+
+import static com.gtnewhorizons.angelica.models.CubeModel.INSTANCE;
+
 @Mixin(BlockLeaves.class)
 public abstract class MixinBlockLeaves extends Block implements QuadProvider {
 
-    // TODO: Use modern model API
-    private static final ThreadLocal<QuadProvider> model = ThreadLocal.withInitial(() -> new CubeModel());
-
-    public List<Quad> getQuads(IBlockAccess world, BlockPos pos, Block block, int meta, ForgeDirection dir, Random random, ObjectPooler<Quad> quadPool) {
-        return model.get().getQuads(world, pos, block, meta, dir, random, quadPool);
+    public List<Quad> getQuads(IBlockAccess world, BlockPos pos, Block block, int meta, ForgeDirection dir, Random random, int color, ObjectPooler<Quad> quadPool) {
+        return INSTANCE.get().getQuads(world, pos, block, meta, dir, random, color, quadPool);
     }
 
     private MixinBlockLeaves(Material materialIn) {
--- src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/models/MixinBlockWorkbench.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/models/MixinBlockWorkbench.java
@@ -1,23 +1,24 @@
 package com.gtnewhorizons.angelica.mixins.early.angelica.models;
 
+import com.gtnewhorizons.angelica.api.QuadProvider;
 import com.gtnewhorizons.angelica.compat.mojang.BlockPos;
 import com.gtnewhorizons.angelica.compat.nd.Quad;
-import com.gtnewhorizons.angelica.utils.ObjectPooler;
-import java.util.List;
-import java.util.Random;
 import com.gtnewhorizons.angelica.models.CubeModel;
-import com.gtnewhorizons.angelica.api.QuadProvider;
+import com.gtnewhorizons.angelica.utils.ObjectPooler;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockWorkbench;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.common.util.ForgeDirection;
 import org.spongepowered.asm.mixin.Mixin;
 
+import java.util.List;
+import java.util.Random;
+
 @Mixin(BlockWorkbench.class)
 public class MixinBlockWorkbench implements QuadProvider {
 
     // TODO: Use modern model API
-    public List<Quad> getQuads(IBlockAccess world, BlockPos pos, Block block, int meta, ForgeDirection dir, Random random, ObjectPooler<Quad> quadPool) {
-        return CubeModel.INSTANCE.get().getQuads(world, pos, block, meta, dir, random, quadPool);
+    public List<Quad> getQuads(IBlockAccess world, BlockPos pos, Block block, int meta, ForgeDirection dir, Random random, int color, ObjectPooler<Quad> quadPool) {
+        return CubeModel.INSTANCE.get().getQuads(world, pos, block, meta, dir, random, color, quadPool);
     }
 }
