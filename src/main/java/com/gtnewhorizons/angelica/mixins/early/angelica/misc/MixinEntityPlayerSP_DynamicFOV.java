e5ef91c9b6d6383b1ccaa3342faa836f6b543922
Caedis <Caedis@users.noreply.github.com>
2024-03-08 10:09:22 -0600

    Revert "scratch that, override return value entirely"
    This reverts commit 20a379d766dc6764a852fdf6cbb2d5413c7b4b88.

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/misc/MixinEntityPlayerSP_DynamicFOV.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/angelica/misc/MixinEntityPlayerSP_DynamicFOV.java
@@ -5,16 +5,17 @@ import jss.notfine.core.Settings;
 import net.minecraft.client.entity.EntityPlayerSP;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
 @Mixin(EntityPlayerSP.class)
 public class MixinEntityPlayerSP_DynamicFOV {
 
-    @Inject(method = "getFOVMultiplier", at = @At("RETURN"), cancellable = true)
-    void angelica$getFOVMultiplier_return(CallbackInfoReturnable<Float> cir) {
-        if (!(boolean)Settings.DYNAMIC_FOV.option.getStore())
-            cir.setReturnValue(1.0f);
+    @ModifyVariable(method = "getFOVMultiplier", at = @At(value = "STORE", ordinal = 2))
+    float angelica$getFOVMultiplier(float f){
+        if ((boolean)Settings.DYNAMIC_FOV.option.getStore())
+            return f;
+
+        return 1.0f;
     }
 
 }
