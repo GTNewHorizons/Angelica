d8d8cd93c564f4fe0dad4c1956dafe1f570b287d
jss2a98aj <jss2a98aj@gmail.com>
2024-05-05 14:30:40 -0400

    Leaf render mode edge case handling

--- src/main/java/com/gtnewhorizons/angelica/mixins/late/notfine/leaves/twilightforest/MixinBlockTFLeaves.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/late/notfine/leaves/twilightforest/MixinBlockTFLeaves.java
@@ -1,12 +1,8 @@
 package com.gtnewhorizons.angelica.mixins.late.notfine.leaves.twilightforest;
 
-import jss.notfine.core.Settings;
-import jss.notfine.core.SettingsManager;
-import jss.notfine.gui.options.named.LeavesQuality;
-import jss.notfine.util.ILeafBlock;
+import jss.notfine.util.LeafRenderUtil;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.init.Blocks;
-import net.minecraft.util.Facing;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,16 +14,7 @@ public abstract class MixinBlockTFLeaves extends BlockLeaves {
     @Override
     public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side) {
         int maskedMeta = world.getBlockMetadata(x, y, z) & 3;
-        int renderMode;
-        if(Settings.MODE_LEAVES.option.getStore() == LeavesQuality.SHELLED_FAST) {
-            renderMode = world.getBlock(
-                x + Facing.offsetsXForSide[side],
-                y + Facing.offsetsYForSide[side],
-                z + Facing.offsetsZForSide[side]
-            ) instanceof ILeafBlock ? 1 : 0;
-        } else {
-            renderMode = SettingsManager.leavesOpaque ? 1 : 0;
-        }
+        int renderMode = LeafRenderUtil.selectRenderMode(world, x, y, z, side) ? 1 : 0;
         maskedMeta = maskedMeta > 1 ? 0 : maskedMeta;
         return Blocks.leaves.field_150129_M[renderMode][maskedMeta];
     }
--- src/main/java/com/gtnewhorizons/angelica/mixins/late/notfine/leaves/witchery/MixinBlockWitchLeaves.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/late/notfine/leaves/witchery/MixinBlockWitchLeaves.java
@@ -1,13 +1,10 @@
 package com.gtnewhorizons.angelica.mixins.late.notfine.leaves.witchery;
 
 import com.emoniph.witchery.blocks.BlockWitchLeaves;
-import jss.notfine.core.Settings;
 import jss.notfine.core.SettingsManager;
-import jss.notfine.gui.options.named.LeavesQuality;
-import jss.notfine.util.ILeafBlock;
+import jss.notfine.util.LeafRenderUtil;
 import net.minecraft.block.BlockLeavesBase;
 import net.minecraft.block.material.Material;
-import net.minecraft.util.Facing;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
@@ -29,16 +26,7 @@ public abstract class MixinBlockWitchLeaves extends BlockLeavesBase {
     @Override
     public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side) {
         int maskedMeta = world.getBlockMetadata(x, y, z) & 3;
-        int renderMode;
-        if(Settings.MODE_LEAVES.option.getStore() == LeavesQuality.SHELLED_FAST) {
-            renderMode = world.getBlock(
-                x + Facing.offsetsXForSide[side],
-                y + Facing.offsetsYForSide[side],
-                z + Facing.offsetsZForSide[side]
-            ) instanceof ILeafBlock ? 1 : 0;
-        } else {
-            renderMode = SettingsManager.leavesOpaque ? 1 : 0;
-        }
+        final int renderMode = LeafRenderUtil.selectRenderMode(world, x, y, z, side) ? 1 : 0;
         maskedMeta = maskedMeta > 1 ? 0 : maskedMeta;
         return iconsForModes[renderMode][maskedMeta];
     }
