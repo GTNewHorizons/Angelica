d8e677f51df49730523c8b1032fb01ac588878e0
Jason Mitchell <mitchej@gmail.com>
2024-05-05 17:25:00 -0700

    Misc cleanup

--- src/main/java/net/coderbot/batchedentityrendering/impl/BufferBuilderExt.java
+++
@@ -1,9 +0,0 @@
-package net.coderbot.batchedentityrendering.impl;
-
-import com.gtnewhorizons.angelica.compat.toremove.DrawState;
-
-import java.nio.ByteBuffer;
-
-public interface BufferBuilderExt {
-
-}
