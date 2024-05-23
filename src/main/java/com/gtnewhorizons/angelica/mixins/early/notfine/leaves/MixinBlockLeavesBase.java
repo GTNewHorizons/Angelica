e1879d8558a94e8d4bb980054a14f7766efb35ff
jss2a98aj <jss2a98aj@gmail.com>
2024-05-23 14:43:11 -0400

    Updated file overlooked in previous sync

--- src/main/java/com/gtnewhorizons/angelica/mixins/early/notfine/leaves/MixinBlockLeavesBase.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/early/notfine/leaves/MixinBlockLeavesBase.java
@@ -1,5 +1,6 @@
 package com.gtnewhorizons.angelica.mixins.early.notfine.leaves;
 
+import jss.notfine.util.IFaceObstructionCheckHelper;
 import jss.notfine.util.ILeafBlock;
 import jss.notfine.util.LeafRenderUtil;
 import net.minecraft.block.Block;
@@ -9,11 +10,16 @@ import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(value = BlockLeavesBase.class)
-public abstract class MixinBlockLeavesBase extends Block implements ILeafBlock {
+public abstract class MixinBlockLeavesBase extends Block implements IFaceObstructionCheckHelper, ILeafBlock {
 
     @Override
     public boolean shouldSideBeRendered(IBlockAccess world, int x, int y, int z, int side) {
         return LeafRenderUtil.shouldSideBeRendered(world, x, y, z, side);
+    }
+
+    @Override()
+    public boolean isFaceNonObstructing(IBlockAccess world, int x, int y, int z, int side, double otherMinX, double otherMinY, double otherMinZ, double otherMaxX, double otherMaxY, double otherMaxZ) {
+        return LeafRenderUtil.isFaceNonObstructing(world, x, y, z);
     }
 
     private MixinBlockLeavesBase(Material material) {
