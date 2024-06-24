a414a4d1e00146e039db7451bff61dc0e379d919
James Larrowe <larrowe.semaj11@gmail.com>
2024-06-24 19:23:12 -0400

    Rewrite as injector
    IDE go brr

--- src/main/java/com/gtnewhorizons/angelica/mixins/late/client/minefactoryreloaded/MixinRedNetCableRenderer.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/late/client/minefactoryreloaded/MixinRedNetCableRenderer.java
@@ -6,36 +6,26 @@ import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.world.IBlockAccess;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import powercrystals.minefactoryreloaded.render.block.RedNetCableRenderer;
-import powercrystals.minefactoryreloaded.setup.MFRConfig;
 import powercrystals.minefactoryreloaded.tile.rednet.TileEntityRedNetCable;
 
-@Mixin(value = RedNetCableRenderer.class, remap = false)
+@Mixin(value = RedNetCableRenderer.class)
 public abstract class MixinRedNetCableRenderer extends TileEntitySpecialRenderer {
-
-    @Shadow
-    private boolean renderCable(IBlockAccess world, int x, int y, int z, int brightness, TileEntityRedNetCable _cable) {
-        throw new AbstractMethodError("Shadow");
-    }
 
     /**
      * @author JL2210
-     * @reason Thread safety compat (check getTileEntity for null)
+     * @reason Thread safety compat (check result of getTileEntity for null)
      */
-    @Overwrite
-    public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId,
-            RenderBlocks renderer) {
-        TileEntityRedNetCable _cable = (TileEntityRedNetCable) world.getTileEntity(x, y, z);
-
-        // rewrite as injector?
-        // orig: if (MFRConfig.TESRCables && _cable.onRender())
-        if (_cable == null || (MFRConfig.TESRCables && _cable.onRender())) return false;
-
-        int brightness = block.getMixedBrightnessForBlock(world, x, y, z);
-
-        return renderCable(world, x, y, z, brightness, _cable);
+    @Inject(method = "renderWorldBlock", at = @At(value = "FIELD", target = "Lpowercrystals/minefactoryreloaded/setup/MFRConfig;TESRCables*:Z"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true, remap = false)
+    public void angelica$checkNullBeforeAccess(IBlockAccess world, int x, int y, int z, Block block, int modelId,
+            RenderBlocks renderer, CallbackInfoReturnable<Boolean> cir, TileEntityRedNetCable _cable) {
+        if (_cable == null) {
+             cir.setReturnValue(false);
+        }
     }
 }
