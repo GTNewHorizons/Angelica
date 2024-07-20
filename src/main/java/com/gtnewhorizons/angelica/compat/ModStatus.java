9f2d164bfba30527b8ae5978735fff92c673a354
Caedis <Caedis@users.noreply.github.com>
2024-07-19 21:58:45 -0500

    Fix Lens Selector Rendering

--- src/main/java/com/gtnewhorizons/angelica/compat/ModStatus.java
+++ src/main/java/com/gtnewhorizons/angelica/compat/ModStatus.java
@@ -27,6 +27,7 @@ public class ModStatus {
     public static boolean isBattlegearLoaded;
     public static boolean isBackhandLoaded;
     public static boolean isThaumcraftLoaded;
+    public static boolean isThaumicHorizonsLoaded;
 
     public static void preInit(){
         isBetterCrashesLoaded = Loader.isModLoaded("bettercrashes");
@@ -40,6 +41,7 @@ public class ModStatus {
         isBattlegearLoaded = Loader.isModLoaded("battlegear2");
         isBackhandLoaded = Loader.isModLoaded("backhand");
         isThaumcraftLoaded = Loader.isModLoaded("Thaumcraft");
+        isThaumicHorizonsLoaded = Loader.isModLoaded("ThaumicHorizons");
 
         if (isHoloInventoryLoaded){
             isHoloInventoryLoaded = new DefaultArtifactVersion("2.4.4-GTNH")
