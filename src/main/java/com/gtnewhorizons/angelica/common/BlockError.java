b153bd621fe6b7a7c07c9a0f31846859b13730e5
RecursivePineapple <recursive_pineapple@proton.me>
2024-10-22 15:45:51 -0400

    Replace BlockError mixin with event handler
    Something is calling TextureMap.registerIcons before preInit() sets up
    the BlockError reference. While adding a null check to the mixin would
    also fix this, a mixin isn't needed here since forge exposes an event
    for this purpose.

--- src/main/java/com/gtnewhorizons/angelica/common/BlockError.java
+++ src/main/java/com/gtnewhorizons/angelica/common/BlockError.java
@@ -2,17 +2,17 @@ package com.gtnewhorizons.angelica.common;
 
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.util.IIcon;
+import net.minecraftforge.client.event.TextureStitchEvent;
+import net.minecraftforge.common.MinecraftForge;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * This Block is not actually getting registered. A static instance is created in AngelicaMod.
- * In order to register the texture for it, there is a mixin to TextureMap which injects a call
- * to this instance's registerBlocKIcons method. Actually registering the block on the client but
- * not the server can cause block IDs to different between client/server, which is not good.
+ * Actually registering the block on the client but not the server can cause block IDs to 
+ * different between client/server, which is not good.
 
  * We're essentially injecting the block's texture, so that we may use this block as if it were
  * any normal block for rendering purposes with RenderBlocks and such, but not have to actually
@@ -25,21 +25,23 @@ public class BlockError extends Block {
 
     public BlockError() {
         super(Material.rock);
+
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
-    @Override
-    @SideOnly(Side.CLIENT)
-    public void registerBlockIcons(IIconRegister reg)
-    {
-        for (int i = 0; i < icons.length; i++) {
-            icons[i] = reg.registerIcon("angelica:error_block_" + i);
+    @SubscribeEvent
+    public void onTextureStitch(TextureStitchEvent.Pre event) {
+        if (event.map.getTextureType() == 0) {
+            for (int i = 0; i < icons.length; i++) {
+                icons[i] = event.map.registerIcon("angelica:error_block_" + i);
+            }
         }
     }
 
     @Override
     @SideOnly(Side.CLIENT)
     public IIcon getIcon(int side, int meta) {
-        return icons[meta];
+        return meta >= 0 && meta < icons.length ? icons[meta] : null;
     }
 
 }
