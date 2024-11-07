66181ab8665bf80e90e41dfd2372debc51c69d38
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-11-07 01:37:16 +0100

    modify zoom with mouse scrolling

--- src/main/java/com/gtnewhorizons/angelica/zoom/Zoom.java
+++ src/main/java/com/gtnewhorizons/angelica/zoom/Zoom.java
@@ -2,6 +2,7 @@ package com.gtnewhorizons.angelica.zoom;
 
 import lombok.Getter;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.MathHelper;
 
 public class Zoom {
 
@@ -10,4 +11,8 @@ public class Zoom {
     @Getter
     private static final KeyBinding zoomKey = new KeyBinding("Zoom", 0, "key.categories.misc");
 
+    public static void modifyZoom(int eventDWheel) {
+        if (eventDWheel == 0) return;
+        zoom = MathHelper.clamp_float(zoom + Integer.signum(eventDWheel), 1.0F, 16.0F);
+    }
 }
--- src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/zoom/MixinMinecraft_Zoom.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/zoom/MixinMinecraft_Zoom.java
@@ -1,10 +1,21 @@
 package com.gtnewhorizons.angelica.mixins.early.angelica.zoom;
 
+import com.gtnewhorizons.angelica.zoom.Zoom;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(Minecraft.class)
 public class MixinMinecraft_Zoom {
 
+    @ModifyExpressionValue(method = "runTick", at = @At(value = "INVOKE", target = "Lorg/lwjgl/input/Mouse;getEventDWheel()I"))
+    private int angelica$captureMouseWheel(int original) {
+        if (Zoom.getZoomKey().getIsKeyPressed()) {
+            Zoom.modifyZoom(original);
+            return 0;
+        }
+        return original;
+    }
 
 }
