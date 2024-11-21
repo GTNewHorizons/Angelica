67a4889983873d737297899cc0c49fba1bf8b834
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-11-20 19:04:24 -0500

    Sleep before swap

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinMinecraft.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinMinecraft.java
@@ -2,6 +2,7 @@ package com.gtnewhorizons.angelica.mixins.early.angelica;
 
 import com.gtnewhorizons.angelica.AngelicaMod;
 import com.gtnewhorizons.angelica.mixins.interfaces.IGameSettingsExt;
+import java.util.concurrent.locks.LockSupport;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.GameSettings;
 import org.lwjgl.input.Keyboard;
@@ -11,12 +12,19 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
-public class MixinMinecraft {
+public abstract class MixinMinecraft {
     @Shadow
     public GameSettings gameSettings;
+
+    @Shadow
+    public abstract boolean isFramerateLimitBelowMax();
+
+    @Shadow
+    public abstract int getLimitFramerate();
 
     @Unique
     private long angelica$lastFrameTime = 0;
@@ -31,6 +39,20 @@ public class MixinMinecraft {
 
     @Inject(
         method = "func_147120_f",
+        at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;update()V", shift = At.Shift.BEFORE, remap = false)
+    )
+    private void angelica$limitFPS(CallbackInfo ci) {
+        if (isFramerateLimitBelowMax()) {
+            final long target = angelica$lastFrameTime + (long) (1.0 / getLimitFramerate() * 1_000_000) * 1_000;
+            long sleepNanos;
+            while ((sleepNanos = target - System.nanoTime()) > 100) {
+                LockSupport.parkNanos(sleepNanos);
+            }
+        }
+    }
+
+    @Inject(
+        method = "func_147120_f",
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;update()V", shift = At.Shift.AFTER, remap = false)
     )
     private void angelica$trackFrametimes(CallbackInfo ci) {
@@ -40,6 +62,9 @@ public class MixinMinecraft {
         AngelicaMod.proxy.putFrametime(time - angelica$lastFrameTime);
         angelica$lastFrameTime = time;
     }
+
+    @Redirect(method = "runGameLoop", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/Display;sync(I)V"))
+    private void angelica$noopFPSLimiter(int fps) {}
 
     @Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiScreen;isShiftKeyDown()Z", shift = At.Shift.AFTER))
     private void angelica$setShowFpsGraph(CallbackInfo ci) {
