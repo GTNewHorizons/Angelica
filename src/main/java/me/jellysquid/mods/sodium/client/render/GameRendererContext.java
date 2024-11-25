6b045bc12197d417021775c3b58aab79a3ae03a8
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2024-11-24 23:41:01 -0500

    Remove another direct buffer creation

--- src/main/java/me/jellysquid/mods/sodium/client/render/GameRendererContext.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/GameRendererContext.java
@@ -1,13 +1,12 @@
 package me.jellysquid.mods.sodium.client.render;
 
+import com.gtnewhorizons.angelica.compat.lwjgl.MemoryStack;
 import com.gtnewhorizons.angelica.compat.toremove.MatrixStack;
 import com.gtnewhorizons.angelica.config.AngelicaConfig;
 import com.gtnewhorizons.angelica.rendering.RenderingState;
+import java.nio.FloatBuffer;
 import net.coderbot.iris.shadows.ShadowRenderingState;
 import org.joml.Matrix4f;
-import org.lwjgl.BufferUtils;
-
-import java.nio.FloatBuffer;
 
 public class GameRendererContext {
     /**
@@ -17,8 +16,8 @@ public class GameRendererContext {
      * @return A float-buffer on the stack containing the model-view-projection matrix in a format suitable for
      * uploading as uniform state
      */
-    public static FloatBuffer getModelViewProjectionMatrix(MatrixStack.Entry matrices) {
-        final FloatBuffer bufModelViewProjection = BufferUtils.createFloatBuffer(16);
+    public static FloatBuffer getModelViewProjectionMatrix(MatrixStack.Entry matrices, MemoryStack stack) {
+        final FloatBuffer bufModelViewProjection = stack.mallocFloat(16);
         final Matrix4f projectionMatrix = (AngelicaConfig.enableIris && ShadowRenderingState.areShadowsCurrentlyBeingRendered()) ? ShadowRenderingState.getShadowOrthoMatrix() : RenderingState.INSTANCE.getProjectionMatrix();
         final Matrix4f matrix = new Matrix4f(projectionMatrix);
         matrix.mul(matrices.getModel());
