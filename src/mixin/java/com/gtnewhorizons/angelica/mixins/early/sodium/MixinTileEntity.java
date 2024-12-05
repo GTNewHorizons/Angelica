0192c02215a13b14635d508e9ec6dde46de8ade1
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2024-12-05 19:36:04 +0100

    cache AxisAlignedBB for tile entity render frustrum check

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/early/sodium/MixinTileEntity.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/sodium/MixinTileEntity.java
@@ -1,20 +1,41 @@
 package com.gtnewhorizons.angelica.mixins.early.sodium;
 
+import com.gtnewhorizons.angelica.mixins.interfaces.TileEntityExt;
 import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 
 @Mixin(TileEntity.class)
-public class MixinTileEntity {
-    @Shadow private Block blockType;
-    @Shadow private int blockMetadata;
-    @Shadow World worldObj;
-    @Shadow int xCoord;
-    @Shadow int yCoord;
-    @Shadow int zCoord;
+public abstract class MixinTileEntity implements TileEntityExt {
+
+    @Shadow
+    public Block blockType;
+    @Shadow
+    public int blockMetadata;
+    @Shadow
+    protected World worldObj;
+    @Shadow
+    public int xCoord;
+    @Shadow
+    public int yCoord;
+    @Shadow
+    public int zCoord;
+    @Unique
+    private int sodium$renderxCoord = -1;
+    @Unique
+    private int sodium$renderyCoord = -1;
+    @Unique
+    private int sodium$renderzCoord = -1;
+    @Unique
+    private AxisAlignedBB sodium$cachedRenderAABB;
+
+    @Shadow
+    public abstract AxisAlignedBB getRenderBoundingBox();
 
     /**
      * @author mitchej123
@@ -26,7 +47,6 @@ public class MixinTileEntity {
         if (block == null) {
             this.blockType = block = this.worldObj.getBlock(this.xCoord, this.yCoord, this.zCoord);
         }
-
         return block;
     }
 
@@ -40,7 +60,20 @@ public class MixinTileEntity {
         if (metadata == -1) {
             this.blockMetadata = metadata = this.worldObj.getBlockMetadata(this.xCoord, this.yCoord, this.zCoord);
         }
-
         return metadata;
+    }
+
+    // cache the render bounding box, otherwise the vanilla code
+    // creates a new AxisAlignedBB object everytime and that
+    // spams allocations
+    @Override
+    public AxisAlignedBB sodium$getCachedRenderBoundingBox() {
+        if (sodium$cachedRenderAABB == null || xCoord != sodium$renderxCoord || yCoord != sodium$renderyCoord || zCoord != sodium$renderzCoord) {
+            sodium$renderxCoord = xCoord;
+            sodium$renderyCoord = yCoord;
+            sodium$renderzCoord = zCoord;
+            sodium$cachedRenderAABB = this.getRenderBoundingBox();
+        }
+        return sodium$cachedRenderAABB;
     }
 }
