d412bee457f68177a0a830c08dc1f754c95b3ddf
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-12-16 17:30:19 -0500

    Properly fix terrain rendering far from origin

--- src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/BlockRenderer.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/pipeline/BlockRenderer.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Random;
 
 public class BlockRenderer {
+    private static final BlockPos POS_ZERO = new BlockPos(0, 0, 0);
 
     private final Random random = new XoRoShiRoRandom();
 
@@ -97,9 +98,10 @@ public class BlockRenderer {
                 TessellatorManager.startCapturing();
                 final CapturingTessellator tess = (CapturingTessellator) TessellatorManager.get();
                 tess.startDrawingQuads();
-                // RenderBlocks adds the subchunk-relative coordinates as the offset, cancel it out here
-
-                tess.setOffset(pos);
+                // Use setTranslation rather than setOffset so that the float data written to the internal buffer
+                // is done in subchunk-relative coordinates
+                tess.setOffset(POS_ZERO);
+                tess.setTranslation(-pos.x, -pos.y, -pos.z);
                 renderBlocks.renderBlockByRenderType(block, pos.x, pos.y, pos.z);
                 final List<QuadView> quads = TessellatorManager.stopCapturingToPooledQuads();
                 tess.resetOffset();
