0d1b05d69081d37512e24ea6850c6dc3e4eef1f6
CookieBrigade <138534411+CookieBrigade@users.noreply.github.com>
2024-12-18 19:58:11 -0600

    Expose for blocks as well

--- src/main/java/com/gtnewhorizons/angelica/api/TextureServices.java
+++ src/main/java/com/gtnewhorizons/angelica/api/TextureServices.java
@@ -2,11 +2,15 @@ package com.gtnewhorizons.angelica.api;
 
 import com.gtnewhorizons.angelica.utils.AnimationsRenderUtils;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 
 /**
  * Exposes a method to the API that allow mods to add texture update compat
  */
 public class TextureServices {
+    public static void updateBlockTextureAnimation(IIcon icon, IBlockAccess blockAccess){
+        AnimationsRenderUtils.markBlockTextureForUpdate(icon);
+    }
     public static void updateTextureAnimation(IIcon icon){
         AnimationsRenderUtils.markBlockTextureForUpdate(icon);
     }
