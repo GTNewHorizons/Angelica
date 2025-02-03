720aaca1b73c52839c024f9181e6547b78d9300a
Fabian Maurer <dark.shadow4@web.de>
2025-02-03 16:13:53 +0100

    Add functionality to fake blockId for shaders

--- src/main/java/me/jellysquid/mods/sodium/client/render/chunk/format/sfp/SFPModelVertexBufferWriterNio.java
+++ src/main/java/me/jellysquid/mods/sodium/client/render/chunk/format/sfp/SFPModelVertexBufferWriterNio.java
@@ -13,7 +13,7 @@ public class SFPModelVertexBufferWriterNio extends VertexBufferWriterNio impleme
     }
 
     @Override
-    public void writeQuad(float x, float y, float z, int color, float u, float v, int light) {
+    public void writeQuad(float x, float y, float z, int color, float u, float v, int light, int shaderBlockId) {
         int i = this.writeOffset;
 
         ByteBuffer buffer = this.byteBuffer;
