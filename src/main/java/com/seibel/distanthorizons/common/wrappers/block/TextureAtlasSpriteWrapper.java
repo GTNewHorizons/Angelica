59914c4f6a6fc36e38277f28d2397593fe0fed75
Fabian Maurer <dark.shadow4@web.de>
2025-03-03 22:17:53 +0100

    y

--- src/main/java/com/seibel/distanthorizons/common/wrappers/block/TextureAtlasSpriteWrapper.java
+++ src/main/java/com/seibel/distanthorizons/common/wrappers/block/TextureAtlasSpriteWrapper.java
@@ -21,13 +21,6 @@ package com.seibel.distanthorizons.common.wrappers.block;
 
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 
-#if MC_VER < MC_1_17_1
-#elif MC_VER < MC_1_21_3
-#else
-import com.seibel.distanthorizons.core.util.ColorUtil;
-import net.minecraft.client.renderer.texture.SpriteContents;
-#endif
-
 /**
  * For wrapping/utilizing around TextureAtlasSprite
  *
@@ -37,42 +30,7 @@ public class TextureAtlasSpriteWrapper
 {
 	public static int getPixelRGBA(TextureAtlasSprite sprite, int frameIndex, int x, int y)
 	{
-        #if MC_VER < MC_1_17_1
-        return sprite.mainImage[0].getPixelRGBA(
-                x + sprite.framesX[frameIndex] * sprite.getWidth(),
-                y + sprite.framesY[frameIndex] * sprite.getHeight());
-        #elif MC_VER < MC_1_19_4
-		if (sprite.animatedTexture != null)
-		{
-			x += sprite.animatedTexture.getFrameX(frameIndex) * sprite.width;
-			y += sprite.animatedTexture.getFrameY(frameIndex) * sprite.height;
-		}
-		return sprite.mainImage[0].getPixelRGBA(x, y);
-		#elif MC_VER < MC_1_21_3
-		if (sprite.contents().animatedTexture != null)
-		{
-			x += sprite.contents().animatedTexture.getFrameX(frameIndex) * sprite.contents().width();
-			y += sprite.contents().animatedTexture.getFrameY(frameIndex) * sprite.contents().width();
-		}
-		return sprite.contents().originalImage.getPixelRGBA(x, y);
-        #else
-		
-		SpriteContents content = sprite.contents(); // don't close, otherwise MC will be corrupted and you won't be able to re-access the texture
-		if (content.animatedTexture != null)
-		{
-			x += content.animatedTexture.getFrameX(frameIndex) * content.width();
-			y += content.animatedTexture.getFrameY(frameIndex) * content.width();
-		}
-		
-		int abgr = content.originalImage.getPixel(x, y);
-		// re-pack the color so we can access it normally
-		int a = (abgr & 0xFF000000) >>> 24;
-		int b = (abgr & 0x00FF0000) >>> 16;
-		int g = (abgr & 0x0000FF00) >>> 8;
-		int r = (abgr & 0x000000FF);
-		return ColorUtil.argbToInt(a, r, g, b);
-        #endif
-		
+        return sprite.getFrameTextureData(frameIndex)[y] [x];
 	}
-	
+
 }
--- src/main/java/com/seibel/distanthorizons/common/wrappers/minecraft/MinecraftGLWrapper.java
+++ src/main/java/com/seibel/distanthorizons/common/wrappers/minecraft/MinecraftGLWrapper.java
@@ -19,7 +19,6 @@
 
 package com.seibel.distanthorizons.common.wrappers.minecraft;
 
-import com.mojang.blaze3d.platform.GlStateManager;
 
 import com.seibel.distanthorizons.core.logging.DhLoggerBuilder;
 import com.seibel.distanthorizons.core.wrapperInterfaces.minecraft.IMinecraftGLWrapper;
@@ -29,18 +28,18 @@ import org.lwjgl.opengl.GL32;
 
 
 /**
- * <b>Why does DH often call GL methods twice? </b><br> 
+ * <b>Why does DH often call GL methods twice? </b><br>
  * Once using the base {@link GL32} function and a second time using
- * Minecraft's {@link GlStateManager}?<br><br>
- * 
+ * Minecraft's ?<br><br>
+ *
  * <b>Answer: </b><br>
  * Compatibility and robustness<br>
- * In general all MC rendering should go through MC's {@link GlStateManager},
+ * In general all MC rendering should go through MC's ,
  * however that isn't always the case.
  * So, to prevent issues if a mod (or MC itself) calls a direct GL function
- * instead of the {@link GlStateManager} wrapper, we need to be sure about what the actual
+ * instead of the wrapper, we need to be sure about what the actual
  * set value is (whether setting or getting) and that MC knows what DH has done.
- * This way whether a mod (or MC) is using the {@link GlStateManager} or direct GL calls,
+ * This way whether a mod (or MC) is using the or direct GL calls,
  * they should always have the correct value for anything DH has modified.
  * <br><br>
  * This may slow down some low end GPUs that are driver limited,
@@ -49,189 +48,176 @@ import org.lwjgl.opengl.GL32;
 public class MinecraftGLWrapper implements IMinecraftGLWrapper
 {
 	public static final MinecraftGLWrapper INSTANCE = new MinecraftGLWrapper();
-	
+
 	private static final Logger LOGGER = DhLoggerBuilder.getLogger();
-	
-	
-	
+
+
+
 	/*
     private static final StencilState STENCIL;
 	 */
-	
-	
+
+
 	// scissor //
-	
+
 	/** @see GL32#GL_SCISSOR_TEST */
 	@Override
-	public void enableScissorTest() 
+	public void enableScissorTest()
 	{
 		GL32.glEnable(GL32.GL_SCISSOR_TEST);
-		GlStateManager._enableScissorTest(); 
 	}
 	/** @see GL32#GL_SCISSOR_TEST */
 	@Override
-	public void disableScissorTest() 
-	{ 
+	public void disableScissorTest()
+	{
 		GL32.glDisable(GL32.GL_SCISSOR_TEST);
-		GlStateManager._disableScissorTest(); 
 	}
-	
-	
+
+
 	// stencil //
-//	
+//
 //	/** @see GL32#GL_SCISSOR_TEST */
 //	public void enableScissorTest() { GlStateManager._stencilFunc(); }
 //	/** @see GL32#GL_SCISSOR_TEST */
 //	public void disableScissorTest() { GlStateManager._disableScissorTest(); }
-	
-	
+
+
 	// depth //
-	
+
 	/** @see GL32#GL_DEPTH_TEST */
 	@Override
-	public void enableDepthTest() 
+	public void enableDepthTest()
 	{
 		GL32.glEnable(GL32.GL_DEPTH_TEST);
-		GlStateManager._enableDepthTest(); 
 	}
 	/** @see GL32#GL_DEPTH_TEST */
 	@Override
-	public void disableDepthTest() 
-	{
-		GL32.glDisable(GL32.GL_DEPTH_TEST);
-		GlStateManager._disableDepthTest(); 
-	}
-	
+	public void disableDepthTest() {
+        GL32.glDisable(GL32.GL_DEPTH_TEST);
+    }
+
 	/** @see GL32#glDepthFunc(int)  */
 	@Override
-	public void glDepthFunc(int func) 
-	{ 
+	public void glDepthFunc(int func)
+	{
 		GL32.glDepthFunc(func);
-		GlStateManager._depthFunc(func); 
 	}
-	
+
 	/** @see GL32#glDepthMask(boolean) */
 	@Override
-	public void enableDepthMask() 
+	public void enableDepthMask()
 	{
 		GL32.glDepthMask(true);
-		GlStateManager._depthMask(true); 
 	}
 	/** @see GL32#glDepthMask(boolean) */
 	@Override
-	public void disableDepthMask() 
+	public void disableDepthMask()
 	{
 		GL32.glDepthMask(false);
-		GlStateManager._depthMask(false); 
 	}
-	
-	
+
+
 	// blending //
-	
+
 	/** @see GL32#GL_BLEND */
 	@Override
-	public void enableBlend() 
+	public void enableBlend()
 	{
 		GL32.glEnable(GL32.GL_BLEND);
-		GlStateManager._enableBlend();
 	}
 	/** @see GL32#GL_BLEND */
 	@Override
-	public void disableBlend() 
+	public void disableBlend()
 	{
 		GL32.glDisable(GL32.GL_BLEND);
-		GlStateManager._disableBlend(); 
 	}
-	
+
 	/** @see GL32#glBlendFunc */
 	@Override
-	public void glBlendFunc(int sfactor, int dfactor) 
+	public void glBlendFunc(int sfactor, int dfactor)
 	{
 		GL32.glBlendFunc(sfactor, dfactor);
-		GlStateManager._blendFunc(sfactor, dfactor); 
 	}
 	/** @see GL32#glBlendFuncSeparate */
 	@Override
-	public void glBlendFuncSeparate(int sfactorRGB, int dfactorRGB, int sfactorAlpha, int dfactorAlpha) 
+	public void glBlendFuncSeparate(int sfactorRGB, int dfactorRGB, int sfactorAlpha, int dfactorAlpha)
 	{
 		GL32.glBlendFuncSeparate(sfactorRGB, dfactorRGB, sfactorAlpha, dfactorAlpha);
-		GlStateManager._blendFuncSeparate(sfactorRGB, dfactorRGB, sfactorAlpha, dfactorAlpha); 
 	}
-	
-	
+
+
 	// frame buffers //
-	
+
 	/** @see GL32#glBindFramebuffer */
 	@Override
-	public void glBindFramebuffer(int target, int framebuffer) 
+	public void glBindFramebuffer(int target, int framebuffer)
 	{
 		GL32.glBindFramebuffer(target, framebuffer);
-		GlStateManager._glBindFramebuffer(target, framebuffer); 
 	}
-	
-	
+
+
 	// buffers //
-	
+
 	/** @see GL32#glGenBuffers() */
 	@Override
 	public int glGenBuffers()
 	{ return GlStateManager._glGenBuffers(); }
-	
+
 	/** @see GL32#glDeleteBuffers(int)  */
 	@Override
 	public void glDeleteBuffers(int buffer)
 	{ GlStateManager._glDeleteBuffers(buffer); }
-	
-	
+
+
 	// culling //
-	
+
 	/** @see GL32#GL_CULL_FACE */
 	@Override
-	public void enableFaceCulling() 
+	public void enableFaceCulling()
 	{
 		GL32.glEnable(GL32.GL_CULL_FACE);
-		GlStateManager._enableCull(); 
+		GlStateManager._enableCull();
 	}
 	/** @see GL32#GL_CULL_FACE */
 	@Override
-	public void disableFaceCulling() 
+	public void disableFaceCulling()
 	{
 		GL32.glDisable(GL32.GL_CULL_FACE);
-		GlStateManager._disableCull(); 
+		GlStateManager._disableCull();
 	}
-	
-	
+
+
 	// textures //
-	
+
 	/** @see GL32#glGenTextures() */
 	@Override
 	public int glGenTextures() { return GlStateManager._genTexture(); }
 	/** @see GL32#glDeleteTextures(int) */
 	@Override
 	public void glDeleteTextures(int texture) { GlStateManager._deleteTexture(texture); }
-	
+
 	/** @see GL32#glActiveTexture(int) */
 	@Override
-	public void glActiveTexture(int textureId) 
-	{ 
+	public void glActiveTexture(int textureId)
+	{
 		GL32.glActiveTexture(textureId);
 		GlStateManager._activeTexture(textureId);
 	}
 	@Override
 	public int getActiveTexture() { return GL32.glGetInteger(GL32.GL_ACTIVE_TEXTURE); }
-	
+
 	/**
 	 * Always binds to {@link GL32#GL_TEXTURE_2D}
 	 * @see GL32#glBindTexture(int, int)
 	 */
 	@Override
-	public void glBindTexture(int texture) 
+	public void glBindTexture(int texture)
 	{
 		GL32.glBindTexture(GL32.GL_TEXTURE_2D, texture);
 		GlStateManager._bindTexture(texture);
 	}
-	
-	
-	
-	
+
+
+
+
 }
