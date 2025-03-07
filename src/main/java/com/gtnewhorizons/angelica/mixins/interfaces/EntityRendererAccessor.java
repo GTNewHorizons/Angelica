1b28570a39f939938961c698d6000898c7a68102
Fabian Maurer <dark.shadow4@web.de>
2025-03-07 06:09:32 +0100

    Implement lightmap and shading, first pink (right color!) LODs

--- src/main/java/com/gtnewhorizons/angelica/mixins/interfaces/EntityRendererAccessor.java
+++ src/main/java/com/gtnewhorizons/angelica/mixins/interfaces/EntityRendererAccessor.java
@@ -1,7 +1,9 @@
 package com.gtnewhorizons.angelica.mixins.interfaces;
 
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.entity.player.EntityPlayer;
 
 public interface EntityRendererAccessor {
     float invokeGetNightVisionBrightness(EntityPlayer entityPlayer, float partialTicks);
+    DynamicTexture getLightmapTexture();
 }
