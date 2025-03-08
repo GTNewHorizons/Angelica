cc4a800d55efec1fb6803e164b2098bb68c0aeeb
Fabian Maurer <dark.shadow4@web.de>
2025-03-08 18:51:39 +0100

    Handle connected events

---
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/NetHandlerPlayClientMixin.java
@@ -0,0 +1,24 @@
+package com.gtnewhorizons.angelica.mixins.early.angelica;
+
+import com.seibel.distanthorizons.core.api.internal.ClientApi;
+import net.minecraft.client.network.NetHandlerPlayClient;
+import net.minecraft.network.play.server.S01PacketJoinGame;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NetHandlerPlayClient.class)
+public class NetHandlerPlayClientMixin {
+    @Inject(method = "handleJoinGame", at = @At("RETURN"))
+    private void connect(S01PacketJoinGame packetIn, CallbackInfo ci)
+    {
+        ClientApi.INSTANCE.onClientOnlyConnected();
+    }
+
+    @Inject(method = "cleanup", at = @At("RETURN"))
+    private void disconnect(CallbackInfo ci)
+    {
+        ClientApi.INSTANCE.onClientOnlyDisconnected();
+    }
+}
