83412e019cbcb41804b8c6887c96a06ed0bdab74
Fabian Maurer <dark.shadow4@web.de>
2025-03-11 05:10:42 +0100

    Hack to work without Iris

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinEntityRenderer.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/MixinEntityRenderer.java
@@ -1,17 +1,28 @@
 package com.gtnewhorizons.angelica.mixins.early.angelica;
 
+import com.gtnewhorizons.angelica.mixins.interfaces.EntityRendererAccessor;
 import com.gtnewhorizons.angelica.rendering.RenderingState;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.texture.DynamicTexture;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderer.class)
-public abstract class MixinEntityRenderer {
+public abstract class MixinEntityRenderer implements EntityRendererAccessor {
+    @Invoker
+    public abstract float invokeGetNightVisionBrightness(EntityPlayer entityPlayer, float partialTicks);
+
+    @Accessor("lightmapTexture")
+    public abstract DynamicTexture getLightmapTexture();
+
     @Inject(method = "setupCameraTransform", at = @At(value = "TAIL"))
     private void angelica$captureCameraMatrix(float partialTicks, int startTime, CallbackInfo ci) {
         final Minecraft mc = Minecraft.getMinecraft();
