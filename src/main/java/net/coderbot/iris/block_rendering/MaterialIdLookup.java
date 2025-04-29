5093487c21399a1c4fa6495825a5daea20b2f295
RecursivePineapple <recursive_pineapple@proton.me>
2025-04-29 15:45:14 -0400

    improve comments

--- src/main/java/net/coderbot/iris/block_rendering/MaterialIdLookup.java
+++ src/main/java/net/coderbot/iris/block_rendering/MaterialIdLookup.java
@@ -2,8 +2,9 @@ package net.coderbot.iris.block_rendering;
 
 import net.minecraft.block.Block;
 
+/** An object which can find material ids for blocks within the context of a shaderpack. */
 public interface MaterialIdLookup {
 
-    /** Looks up a block's material id for the matching shaderpack. */
+    /** Looks up a block's material id for the current shaderpack. */
     short get(Block block, int meta);
 }
