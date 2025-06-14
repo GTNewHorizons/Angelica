33453b3b8c905942a178e650c1f159fbc28a6c4e
jss2a98aj <jss2a98aj@gmail.com>
2025-06-13 20:33:50 -0400

    Fix crash with OpenAutism
    Brandyyn says this fixes the crash but I have not tested it myself.

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/early/notfine/fix/MixinRenderItem.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/notfine/fix/MixinRenderItem.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(value = RenderItem.class)
+@Mixin(value = RenderItem.class, priority = 900)
 public abstract class MixinRenderItem {
 
 	/**
