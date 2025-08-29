45768848983950b7e9ea2508ecda8fefe13e157a
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-08-29 09:52:47 -0400

    Replace old models
    With a single new and interesting model!

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/models/MixinBlock.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/models/MixinBlock.java
@@ -3,11 +3,9 @@ package com.gtnewhorizons.angelica.mixins.early.angelica.models;
 import com.gtnewhorizon.gtnhlib.client.model.BakedModel;
 import com.gtnewhorizons.angelica.config.AngelicaConfig;
 import com.gtnewhorizons.angelica.mixins.interfaces.ModeledBlock;
-import com.gtnewhorizons.angelica.models.VanillaModels;
 import lombok.Getter;
 import lombok.Setter;
 import net.minecraft.block.Block;
-import net.minecraft.init.Blocks;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -26,13 +24,5 @@ public abstract class MixinBlock implements ModeledBlock {
 
         if (!AngelicaConfig.injectQPRendering)
             return;
-
-        ((ModeledBlock) Blocks.stone).setModel(VanillaModels.STONE);
-        ((ModeledBlock) Blocks.glass).setModel(VanillaModels.GLASS);
-        ((ModeledBlock) Blocks.crafting_table).setModel(VanillaModels.WORKBENCH);
-        ((ModeledBlock) Blocks.leaves).setModel(VanillaModels.OLD_LEAF);
-        ((ModeledBlock) Blocks.leaves2).setModel(VanillaModels.NEW_LEAF);
-        //((ModeledBlock) Blocks.log).setModel(VanillaModels.OLD_LOG);
-        //((ModeledBlock) Blocks.log2).setModel(VanillaModels.NEW_LOG);
     }
 }
