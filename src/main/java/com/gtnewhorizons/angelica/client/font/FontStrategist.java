45bed1f5e84012fe04d113aeefdc329fb7696fdc
Death Fuel <108496936+DeathFuel@users.noreply.github.com>
2025-10-05 23:21:38 +0200

    remove duplicate fonts

--- src/main/java/com/gtnewhorizons/angelica/client/font/FontStrategist.java
+++ src/main/java/com/gtnewhorizons/angelica/client/font/FontStrategist.java
@@ -1,20 +1,60 @@
 package com.gtnewhorizons.angelica.client.font;
 
+import com.google.common.collect.HashMultiset;
 import com.gtnewhorizons.angelica.config.FontConfig;
 import com.gtnewhorizons.angelica.mixins.interfaces.ResourceAccessor;
+import lombok.Getter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.DefaultResourcePack;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.io.File;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
 public class FontStrategist {
 
+    @Getter
+    private static final Font[] availableFonts;
+    public static final Logger LOGGER = LogManager.getLogger("Angelica");
+    public static boolean customFontInUse;
+
     static {
+        // get available fonts without duplicates (200 copies of dialog.plain need not apply)
+        Font[] availableFontsDirty = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        HashMap<String, Font> fontSet = new HashMap<>();
+        HashMultiset<String> duplicates = HashMultiset.create(); // for debugging
+
+        for (Font font : availableFontsDirty) {
+            String fontName = font.getFontName();
+            if (fontSet.containsKey(fontName)) {
+                duplicates.add(fontName);
+            } else {
+                fontSet.put(fontName, font);
+            }
+        }
+
+        if (!duplicates.isEmpty()) {
+            StringBuilder sb = new StringBuilder(duplicates.size() + " duplicate font(s) found in the list reported by Java: ");
+            for (Iterator<String> iter = duplicates.iterator(); iter.hasNext(); ) {
+                String dupe = iter.next();
+                sb.append(duplicates.count(dupe)).append("x ").append(dupe);
+                if (iter.hasNext()) {
+                    sb.append(", ");
+                }
+            }
+            LOGGER.warn(sb.toString());
+        }
+
+        availableFonts = fontSet.values().stream().sorted(Comparator.comparing(Font::getFontName)).toArray(Font[]::new);
+
+        // create and add the resource pack that provides fonts
         HashMap<String, File> packMap = new HashMap<>();
         for (int i = 0; i < FontProviderCustom.ATLAS_COUNT; i++) {
             packMap.put(FontProviderCustom.getPrimary().getAtlasResourceName(i), new File(FontProviderCustom.getPrimary().getAtlasFullPath(i)));
@@ -26,7 +66,7 @@ public class FontStrategist {
         defaultResourcePacks.add(fontResourcePack);
         Minecraft.getMinecraft().refreshResources();
 
-        Font[] availableFonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        // determine if a custom font is in use based on config values
         boolean primaryFontFound = false;
         boolean fallbackFontFound = false;
         for (Font availableFont : availableFonts) {
@@ -41,8 +81,6 @@ public class FontStrategist {
         }
         customFontInUse = (FontConfig.enableCustomFont && (primaryFontFound || fallbackFontFound));
     }
-
-    public static boolean customFontInUse;
 
     /**
      Lets you get a FontProvider per char while respecting font priority and fallbacks, the unicode flag, whether or not
@@ -69,7 +107,6 @@ public class FontStrategist {
     }
 
     public static void reloadCustomFontProviders() {
-        Font[] availableFonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
         FontProviderCustom.getPrimary().setFont(null);
         FontProviderCustom.getFallback().setFont(null);
         boolean primaryFontFound = false;
