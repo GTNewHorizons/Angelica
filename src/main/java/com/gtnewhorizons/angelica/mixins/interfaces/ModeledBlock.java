9326fa724fd022bef2d8378456a4fa68fe38ca27
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2025-10-07 12:33:04 -0400

    Fix JSON model detection + remove dead code

--- src/main/java/com/gtnewhorizons/angelica/mixins/interfaces/ModeledBlock.java
+++
@@ -1,9 +0,0 @@
-package com.gtnewhorizons.angelica.mixins.interfaces;
-
-import com.gtnewhorizon.gtnhlib.client.model.BakedModel;
-
-public interface ModeledBlock {
-
-    BakedModel getModel();
-    void setModel(BakedModel model);
-}
