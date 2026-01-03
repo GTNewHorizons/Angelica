55568d2a17655063cbbc5c3d0d2cec85ef93ffe1
Wilhelm Schuster <dev@rot13.io>
2026-01-03 20:39:32 +0100

    Add more menu background textures

--- src/main/java/jss/notfine/gui/options/named/BackgroundSelect.java
+++ src/main/java/jss/notfine/gui/options/named/BackgroundSelect.java
@@ -12,11 +12,18 @@ public enum BackgroundSelect implements NamedState {
 
     DEFAULT("generator.default", (Gui.optionsBackground != null) ? Gui.optionsBackground : new ResourceLocation("textures/gui/options_background.png")),
     SAND("tile.sand.default.name", new ResourceLocation("textures/blocks/sand.png")),
+    GRAVEL("tile.gravel.name", new ResourceLocation("textures/blocks/gravel.png")),
     MYCELIUM("tile.mycel.name", new ResourceLocation("textures/blocks/mycelium_top.png")),
+    STONE("tile.stone.name", new ResourceLocation("textures/blocks/stone.png")),
     STONEBRICK("tile.stonebricksmooth.name", new ResourceLocation("textures/blocks/stonebrick.png")),
     MOSSY_STONEBRICK("tile.stonebricksmooth.mossy.name", new ResourceLocation("textures/blocks/stonebrick_mossy.png")),
+    COBBLESTONE("tile.stonebrick.name", new ResourceLocation("textures/blocks/cobblestone.png")),
     OAK_PLANKS("tile.wood.oak.name", new ResourceLocation("textures/blocks/planks_oak.png")),
-    BIRCH_PLANKS("tile.wood.birch.name", new ResourceLocation("textures/blocks/planks_birch.png"));
+    BIRCH_PLANKS("tile.wood.birch.name", new ResourceLocation("textures/blocks/planks_birch.png")),
+    NETHER_BRICKS("tile.netherBrick.name", new ResourceLocation("textures/blocks/nether_brick.png")),
+    SOUL_SAND("tile.hellsand.name", new ResourceLocation("textures/blocks/soul_sand.png")),
+    SPONGE("tile.sponge.name", new ResourceLocation("textures/blocks/sponge.png")),
+    OBSIDIAN("tile.obsidian.name", new ResourceLocation("textures/blocks/obsidian.png"));
 
     private final String name;
     private final ResourceLocation texture;
