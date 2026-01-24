750e3632fc6f05d3ba4879b561e32252bd182fbf
Jason Mitchell <mitchej@gmail.com>
2026-01-24 12:19:43 -0800

    Lighting fixes * Update emissive check to only include getMixedBrightnessForBlock and not lu * Fixes blocks next to emissives and sadly stops mushrooms from being emissive :(

--- src/main/java/com/gtnewhorizons/angelica/rendering/celeritas/light/LightDataCache.java
+++ src/main/java/com/gtnewhorizons/angelica/rendering/celeritas/light/LightDataCache.java
@@ -35,17 +35,6 @@ public class LightDataCache extends LightDataAccess {
         // Opacity check - true if block is view-blocking (solid blocks)
         final boolean op = block.getMaterial().isOpaque() && block.getLightOpacity() > 0;
 
-        boolean em;
-        if (lu > 0) {
-            em = true;
-        } else {
-            try {
-                em = block.getMixedBrightnessForBlock(EmptyBlockAccess.INSTANCE, x, y, z) == QuadLightingHelper.FULL_BRIGHT_15;
-            } catch (Exception e) {
-                em = false;
-            }
-        }
-
-        return packFC(fc) | packFO(fo) | packOP(op) | packEM(em) | packAO(ao) | packLU(lu) | packSL(sl) | packBL(bl);
+        return packFC(fc) | packFO(fo) | packOP(op) | packAO(ao) | packLU(lu) | packSL(sl) | packBL(bl);
     }
 }
