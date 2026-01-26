46602ab921dfba68718e3642fb4144ae3ac65bc9
Jason Mitchell <mitchej@gmail.com>
2026-01-25 19:58:33 -0800

    Chunk Thread Saftey (#1320)

---
+++ src/main/java/com/gtnewhorizons/angelica/mixins/interfaces/IChunkTileEntityMapHolder.java
@@ -0,0 +1,7 @@
+package com.gtnewhorizons.angelica.mixins.interfaces;
+
+import com.gtnewhorizons.angelica.utils.ConcurrentTileEntityMap;
+
+public interface IChunkTileEntityMapHolder {
+    ConcurrentTileEntityMap angelica$getConcurrentTEMap();
+}
