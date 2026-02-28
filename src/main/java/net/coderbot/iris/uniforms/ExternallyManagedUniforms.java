464dbdd2756a93139d9a8e58047b4ad63537eb62
Jason Mitchell <mitchej@gmail.com>
2026-02-28 14:12:12 -0800

    Stop some warning noise

--- src/main/java/net/coderbot/iris/uniforms/ExternallyManagedUniforms.java
+++ src/main/java/net/coderbot/iris/uniforms/ExternallyManagedUniforms.java
@@ -10,10 +10,15 @@ public class ExternallyManagedUniforms {
 
 	public static void addExternallyManagedUniforms(UniformHolder uniformHolder) {
 		addMat4(uniformHolder, "iris_ModelViewMatrix");
+		addMat4(uniformHolder, "iris_ModelViewMatrixInverse");
 		addMat4(uniformHolder, "iris_ProjectionMatrix");
+		addMat4(uniformHolder, "iris_ProjectionMatrixInverse");
+		addMat4(uniformHolder, "iris_LightmapTextureMatrix");
 		addMat4(uniformHolder, "u_ModelViewProjectionMatrix");
 		addMat3(uniformHolder, "iris_NormalMatrix");
 		uniformHolder.externallyManagedUniform("heavyFog", UniformType.BOOL);
+		uniformHolder.externallyManagedUniform("angelica_ClipPlanesEnabled", UniformType.BOOL);
+		uniformHolder.externallyManagedUniform("angelica_ClipPlane[0]", UniformType.VEC4);
 	}
 
 	public static void addExternallyManagedUniforms116(UniformHolder uniformHolder) {
@@ -21,6 +26,7 @@ public class ExternallyManagedUniforms {
 
 		uniformHolder.externallyManagedUniform("u_ModelScale", UniformType.VEC3);
 		uniformHolder.externallyManagedUniform("u_TextureScale", UniformType.VEC2);
+		uniformHolder.externallyManagedUniform("u_RegionOffset", UniformType.VEC3);
 	}
 
 	private static void addMat3(UniformHolder uniformHolder, String name) {
