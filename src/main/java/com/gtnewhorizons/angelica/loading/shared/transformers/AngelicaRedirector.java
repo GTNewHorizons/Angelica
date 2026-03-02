cf9993fce45ee1f076c72aa15bafd6fde4759d61
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2026-03-03 00:11:34 +0100

    change rfb exclusions (#1440)

--- src/main/java/com/gtnewhorizons/angelica/loading/shared/transformers/AngelicaRedirector.java
+++ src/main/java/com/gtnewhorizons/angelica/loading/shared/transformers/AngelicaRedirector.java
@@ -312,7 +312,6 @@ public final class AngelicaRedirector {
             "org.lwjgl",
             "com.gtnewhorizons.angelica.glsm.",
             "com.gtnewhorizons.angelica.transform",
-            "com.gtnewhorizon.gtnhlib.asm.",
             "me.eigenraven.lwjgl3ify"
         };
     }
