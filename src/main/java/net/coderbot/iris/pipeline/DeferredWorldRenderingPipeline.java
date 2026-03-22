31c44badbe2d328060dbd7008053e3ab859d8a81
Jason Mitchell <mitchej@gmail.com>
2026-03-22 11:05:49 -0700

    Clear cache on reload shaders

--- src/main/java/net/coderbot/iris/pipeline/DeferredWorldRenderingPipeline.java
+++ src/main/java/net/coderbot/iris/pipeline/DeferredWorldRenderingPipeline.java
@@ -45,6 +45,7 @@ import net.coderbot.iris.postprocess.BufferFlipper;
 import net.coderbot.iris.postprocess.CenterDepthSampler;
 import net.coderbot.iris.postprocess.CompositeRenderer;
 import net.coderbot.iris.postprocess.FinalPassRenderer;
+import net.coderbot.iris.postprocess.FullScreenQuadRenderer;
 import net.coderbot.iris.postprocess.ProgramBuildContext;
 import net.coderbot.iris.rendertarget.IRenderTargetExt;
 import net.coderbot.iris.rendertarget.NativeImageBackedSingleColorTexture;
@@ -1028,6 +1029,7 @@ public class DeferredWorldRenderingPipeline implements WorldRenderingPipeline, R
 		BlendModeOverride.restore();
 		AlphaTestOverride.restore();
 
+		FullScreenQuadRenderer.clearLocCache();
 		destroyPasses(table);
 
 		// Destroy the composite rendering pipeline
