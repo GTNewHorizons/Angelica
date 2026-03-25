5a43eef1584e386a96f4af8d2901e75d4d8b951f
Fabian Maurer <dark.shadow4@web.de>
2026-03-25 17:50:42 +0100

    Fix shader pack compatible message

--- src/main/java/net/coderbot/iris/Iris.java
+++ src/main/java/net/coderbot/iris/Iris.java
@@ -757,10 +757,9 @@ public class Iris {
         }
 
         if (loadedIncompatiblePack() && Minecraft.getMinecraft().thePlayer != null) {
-            // Minecraft.getInstance().gui.setTimes(10, 70, 140);
             Iris.logger.warn("Incompatible pack for DH!");
-            // Minecraft.getInstance().gui.setTitle(Component.literal("This pack doesn't have DH support").withStyle(ChatFormatting.BOLD, ChatFormatting.RED));
-            // Minecraft.getInstance().gui.setSubtitle(Component.literal("Distant Horizons (DH) chunks won't show up. This isn't a bug, get another shader.").withStyle(ChatFormatting.RED));
+            Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.BOLD.toString() + EnumChatFormatting.RED + "This pack doesn't have DH support."));
+            Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "Distant Horizons (DH) chunks won't show up. This isn't a bug, get another shader."));
         }
     }
 
