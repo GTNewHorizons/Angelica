69091add8b5d32c7fd100c9ca5f600012a622321
Jason Mitchell <mitchej+github@gmail.com>
2026-04-21 12:46:50 -0700

    Maybe fix DragonAPI on OSX - take two * Bind a default VAO on the SharedDrawable * Mixin that tries to swap out glGetProgramInfoLog when appropriate

---
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/dragonapi/MixinShaderRegistry_ParseError.java
@@ -0,0 +1,19 @@
+package com.gtnewhorizons.angelica.mixins.early.dragonapi;
+
+import org.lwjgl.opengl.GL20;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+
+@Pseudo
+@Mixin(targets = { "Reika/DragonAPI/IO/Shaders/ShaderRegistry" }, remap = false)
+public class MixinShaderRegistry_ParseError {
+
+    @Redirect(method = "parseError",
+        at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL20;glGetShaderInfoLog(II)Ljava/lang/String;"))
+    private static String angelica$routeInfoLogByType(int id, int maxLen) {
+        return GL20.glIsProgram(id) ? GL20.glGetProgramInfoLog(id, maxLen) : GL20.glGetShaderInfoLog(id, maxLen);
+    }
+}
