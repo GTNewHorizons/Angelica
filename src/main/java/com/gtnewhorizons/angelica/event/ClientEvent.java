7915bb0baaa0742a75652e414ff7433be8ed75e2
Midnight <23142346+Midnight145@users.noreply.github.com>
2026-06-23 21:42:40 -0500

    remove varargs in favor of explicit params

--- src/main/java/com/gtnewhorizons/angelica/event/ClientEvent.java
+++ src/main/java/com/gtnewhorizons/angelica/event/ClientEvent.java
@@ -1,22 +1,36 @@
 package com.gtnewhorizons.angelica.event;
 
+import net.minecraft.client.renderer.EntityRenderer;
 import net.minecraftforge.eventbus.api.bus.EventBus;
 import net.minecraftforge.eventbus.api.event.MutableEvent;
 
 public class ClientEvent extends MutableEvent {
     public static final EventBus<ClientEvent> BUS = EventBus.create(ClientEvent.class);
 
-    public ClientEventType event;
-    public Object[] params;
+    public final ClientEventType event;
+    public final EntityRenderer renderer;
+    public final float partialTicks;
 
-    private ClientEvent(ClientEventType event, Object... params) {
+    private ClientEvent(ClientEventType event) {
         this.event = event;
-        this.params = params;
+        this.renderer = null;
+        this.partialTicks = 0;
     }
 
-    public static void post(ClientEventType event, Object... params) {
+    private ClientEvent(ClientEventType event, EntityRenderer renderer, float partialTicks) {
+        this.event = event;
+        this.renderer = renderer;
+        this.partialTicks = partialTicks;
+    }
+
+    public static void post(ClientEventType event, EntityRenderer renderer, float partialTicks) {
         if (!BUS.hasListeners()) return;
-        BUS.post(new ClientEvent(event, params));
+        BUS.post(new ClientEvent(event, renderer, partialTicks));
+    }
+
+    public static void post(ClientEventType event) {
+        if (!BUS.hasListeners()) return;
+        BUS.post(new ClientEvent(event));
     }
 }
 
