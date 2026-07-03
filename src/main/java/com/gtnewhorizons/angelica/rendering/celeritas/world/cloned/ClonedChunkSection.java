17ec643768619ec3b14e8a4c3f57ed569b8c1f10
Jason Mitchell <mitchej@gmail.com>
2026-07-03 14:08:16 -0700

    Conditional LOTR compat

--- src/main/java/com/gtnewhorizons/angelica/rendering/celeritas/world/cloned/ClonedChunkSection.java
+++ src/main/java/com/gtnewhorizons/angelica/rendering/celeritas/world/cloned/ClonedChunkSection.java
@@ -4,6 +4,7 @@ import com.gtnewhorizons.angelica.api.BlockLightProvider;
 import com.gtnewhorizons.angelica.api.SectionLightData;
 import com.gtnewhorizons.angelica.compat.ExtendedBlockStorageExt;
 import com.gtnewhorizons.angelica.compat.ModStatus;
+import com.gtnewhorizons.angelica.compat.lotr.LOTRCompat;
 import com.gtnewhorizons.angelica.compat.mojang.ChunkSectionPos;
 import com.gtnewhorizons.angelica.mixins.interfaces.IChunkTileEntityMapHolder;
 import com.gtnewhorizons.angelica.utils.ConcurrentTileEntityMap;
@@ -19,6 +20,7 @@ import net.minecraft.world.ChunkPosition;
 import net.minecraft.world.EnumSkyBlock;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.biome.WorldChunkManager;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.NibbleArray;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
@@ -27,6 +29,8 @@ import net.minecraftforge.fluids.Fluid;
 public class ClonedChunkSection {
 
     private static final ExtendedBlockStorage EMPTY_SECTION = new ExtendedBlockStorage(0, false);
+    private static final int SECTION_BLOCK_LENGTH = 16;
+    private static final int BIOME_DATA_LENGTH = SECTION_BLOCK_LENGTH * SECTION_BLOCK_LENGTH;
 
     private final ClonedChunkSectionCache backingCache;
     private final World world;
@@ -61,20 +65,28 @@ public class ClonedChunkSection {
         this.pos = pos;
         this.data = new ExtendedBlockStorageExt(chunk, section);
 
-        this.biomeData = new BiomeGenBase[chunk.getBiomeArray().length];
+        this.biomeData = new BiomeGenBase[BIOME_DATA_LENGTH];
 
         copyBlockEntities(chunk, pos);
 
-        fillBiomeData();
+        fillBiomeData(chunk);
 
         this.sectionLightData = BlockLightProvider.getInstance().prepareSectionData(chunk, pos.y);
     }
 
-    private void fillBiomeData() {
-        for (int z = pos.getMinZ(); z <= pos.getMaxZ(); z++) {
-            for (int x = pos.getMinX(); x <= pos.getMaxX(); x++) {
-                int lX = x & 15, lZ = z & 15;
-                this.biomeData[(lZ << 4) | lX] = world.getBiomeGenForCoords(x, z);
+    private void fillBiomeData(Chunk chunk) {
+        if (LOTRCompat.isLotrProvider(world.provider)) {
+            for (int z = pos.getMinZ(); z <= pos.getMaxZ(); z++) {
+                for (int x = pos.getMinX(); x <= pos.getMaxX(); x++) {
+                    this.biomeData[((z & 15) << 4) | (x & 15)] = world.getBiomeGenForCoords(x, z);
+                }
+            }
+        } else {
+            final WorldChunkManager wcm = world.getWorldChunkManager();
+            for (int lz = 0; lz < SECTION_BLOCK_LENGTH; lz++) {
+                for (int lx = 0; lx < SECTION_BLOCK_LENGTH; lx++) {
+                    this.biomeData[(lz << 4) | lx] = chunk.getBiomeGenForWorldCoords(lx, lz, wcm);
+                }
             }
         }
     }
