065b4e408d49d9d365b887ec9afa718adc14a8b7
Logan Phillips <phillips-l@outlook.com>
2026-07-12 11:34:08 -0400

    Get rid of that allocation

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/early/rendering/MixinBlockLiquid.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/rendering/MixinBlockLiquid.java
@@ -2,26 +2,21 @@ package com.gtnewhorizons.angelica.mixins.early.rendering;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
-import net.minecraft.block.material.Material;
 import net.minecraft.util.Vec3;
 import net.minecraft.world.IBlockAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(BlockLiquid.class)
 public class MixinBlockLiquid {
 
-    @Inject(method = "getFlowDirection", at = @At("HEAD"), cancellable = true)
-    private static void angelica$dispatchFlowVectorOnBlock(IBlockAccess world, int x, int y, int z, Material material, CallbackInfoReturnable<Double> cir) {
+    @Redirect(
+        method = "getFlowDirection",
+        at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockLiquid;getFlowVector(Lnet/minecraft/world/IBlockAccess;III)Lnet/minecraft/util/Vec3;"))
+    private static Vec3 angelica$flowVectorOfActualBlock(BlockLiquid singleton, IBlockAccess world, int x, int y, int z) {
         final Block block = world.getBlock(x, y, z);
-        if (!(block instanceof BlockLiquid)) {
-            return;
-        }
-        final Vec3 vec = ((BlockLiquidFlowInvoker) block).angelica$getFlowVector(world, x, y, z);
-        cir.setReturnValue(vec.xCoord == 0.0D && vec.zCoord == 0.0D
-            ? -1000.0D
-            : Math.atan2(vec.zCoord, vec.xCoord) - (Math.PI / 2.0D));
+        final BlockLiquid liquid = block instanceof BlockLiquid ? (BlockLiquid) block : singleton;
+        return ((BlockLiquidFlowInvoker) liquid).angelica$getFlowVector(world, x, y, z);
     }
 }
