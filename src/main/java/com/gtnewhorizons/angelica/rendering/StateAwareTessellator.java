fb06fd7d2aa6c550aa1664df1832c7a78dba7c39
ah-OOG-ah <75745146+ah-OOG-ah@users.noreply.github.com>
2026-08-05 01:17:10 -0400

    Add a flag for no directional shading

--- src/main/java/com/gtnewhorizons/angelica/rendering/StateAwareTessellator.java
+++ src/main/java/com/gtnewhorizons/angelica/rendering/StateAwareTessellator.java
@@ -1,18 +1,19 @@
 package com.gtnewhorizons.angelica.rendering;
 
 public interface StateAwareTessellator {
-    /**
-     * True if the vertex originated from a RenderBlocks method call with the enableAO flag set.
-     */
+
+    /// True if the vertex originated from a RenderBlocks method call with the enableAO flag set.
     int RENDERED_WITH_VANILLA_AO = 0x1;
+    int NO_DIRECTIONAL_SHADING = 0x2;
 
     void angelica$setAppliedAo(boolean flag);
+    /// Used by GTNHLib, possibly others.
+    @SuppressWarnings("Unused")
+    void angelica$setNoDirectionalShading(boolean flag);
 
-    /**
-     * Sets whether we're doing terrain meshing as part of celeritas -- collects additional information.
-     *
-     * Enables per-vertex AO state collection into {@link #angelica$getVertexStates()}.
-     */
+    /// Sets whether we're doing terrain meshing as part of celeritas -- collects additional information.
+    ///
+    /// Enables per-vertex AO state collection into [#angelica$getVertexStates()].
     void angelica$setCeleritasMeshing(boolean active);
 
     int[] angelica$getVertexStates();
