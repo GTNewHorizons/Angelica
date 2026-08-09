0dac59aa7464ba7155a088f9485f700eb1864433
brandyn <sonicbrandyn@gmail.com>
2026-08-09 21:26:36 +0100

    remove Object[] allocation

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/optimizations/MixinRendererLivingEntity.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/early/angelica/optimizations/MixinRendererLivingEntity.java
@@ -1,40 +1,40 @@
 package com.gtnewhorizons.angelica.mixins.early.angelica.optimizations;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumChatFormatting;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(RendererLivingEntity.class)
 public abstract class MixinRendererLivingEntity {
 
-    @WrapOperation(
+    @Redirect(
         method = "rotateCorpse",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/entity/EntityLivingBase;getCommandSenderName()Ljava/lang/String;"),
         require = 0)
-    private static String angelica$skipUnusedEntityName(EntityLivingBase entity, Operation<String> original) {
+    private static String angelica$skipUnusedEntityName(EntityLivingBase entity) {
         if ((entity instanceof EntityLiving living && living.hasCustomNameTag())
             || (entity instanceof EntityPlayer player && !player.getHideCape())) {
-            return original.call(entity);
+            return entity.getCommandSenderName();
         }
 
         return null;
     }
 
-    @WrapOperation(
+    @Redirect(
         method = "rotateCorpse",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/util/EnumChatFormatting;getTextWithoutFormattingCodes(Ljava/lang/String;)Ljava/lang/String;"),
         require = 0)
-    private static String angelica$skipUnusedNameFormatting(String entityName, Operation<String> original) {
-        return entityName == null ? "" : original.call(entityName);
+    private static String angelica$skipUnusedNameFormatting(String entityName) {
+        return entityName == null ? "" : EnumChatFormatting.getTextWithoutFormattingCodes(entityName);
     }
 }
--- src/mixin/java/com/gtnewhorizons/angelica/mixins/late/client/starminer/MixinTransformClientHelper.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/late/client/starminer/MixinTransformClientHelper.java
@@ -1,43 +1,43 @@
 package com.gtnewhorizons.angelica.mixins.late.client.starminer;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumChatFormatting;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Pseudo
 @Mixin(targets = "jp.mc.ancientred.starminer.core.TransformClientHelper", remap = false)
 public abstract class MixinTransformClientHelper {
 
-    @WrapOperation(
+    @Redirect(
         method = "rotateCorpseByGravity",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/entity/EntityLivingBase;getCommandSenderName()Ljava/lang/String;"),
         remap = true,
         require = 0)
-    private static String angelica$skipUnusedEntityName(EntityLivingBase entity, Operation<String> original) {
+    private static String angelica$skipUnusedEntityName(EntityLivingBase entity) {
         if ((entity instanceof EntityLiving living && living.hasCustomNameTag())
             || (entity instanceof EntityPlayer player && !player.getHideCape())) {
-            return original.call(entity);
+            return entity.getCommandSenderName();
         }
 
         return null;
     }
 
-    @WrapOperation(
+    @Redirect(
         method = "rotateCorpseByGravity",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/util/EnumChatFormatting;getTextWithoutFormattingCodes(Ljava/lang/String;)Ljava/lang/String;"),
         remap = true,
         require = 0)
-    private static String angelica$skipUnusedNameFormatting(String entityName, Operation<String> original) {
-        return entityName == null ? "" : original.call(entityName);
+    private static String angelica$skipUnusedNameFormatting(String entityName) {
+        return entityName == null ? "" : EnumChatFormatting.getTextWithoutFormattingCodes(entityName);
     }
 }
