6af401c8344cbbc95d1b6800d4977e1ac9764db8
brandyn <sonicbrandyn@gmail.com>
2026-08-13 19:27:43 +0100

    fix starminer rotation & optimize starminer rotateCorpseByGravity (#2005)
    * fix starminer rotation & optimize starminer rotateCorpseByGravity
    
    fixes #487
    
    * remove Object[] allocation
    
    * add shared helper,  fix NPEs, fallbacks

--- src/mixin/java/com/gtnewhorizons/angelica/mixins/late/client/starminer/MixinTransformClientHelper.java
+++ src/mixin/java/com/gtnewhorizons/angelica/mixins/late/client/starminer/MixinTransformClientHelper.java
@@ -1,31 +1,34 @@
-package com.gtnewhorizons.angelica.mixins.early.angelica.optimizations;
+package com.gtnewhorizons.angelica.mixins.late.client.starminer;
 
 import com.gtnewhorizons.angelica.helpers.RendererLivingEntityHelper;
-import net.minecraft.client.renderer.entity.RendererLivingEntity;
 import net.minecraft.entity.EntityLivingBase;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(RendererLivingEntity.class)
-public class MixinRendererLivingEntity {
+@Pseudo
+@Mixin(targets = "jp.mc.ancientred.starminer.core.TransformClientHelper", remap = false)
+public class MixinTransformClientHelper {
 
     @Redirect(
-        method = "rotateCorpse",
+        method = "rotateCorpseByGravity(Lnet/minecraft/entity/EntityLivingBase;FFF)V",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/entity/EntityLivingBase;getCommandSenderName()Ljava/lang/String;"),
+            target = "Lnet/minecraft/entity/EntityLivingBase;getCommandSenderName()Ljava/lang/String;",
+            remap = true),
         require = 0)
     private static String angelica$skipUnusedEntityName(EntityLivingBase entity) {
         return RendererLivingEntityHelper.getUpsideDownName(entity);
     }
 
     @Redirect(
-        method = "rotateCorpse",
+        method = "rotateCorpseByGravity(Lnet/minecraft/entity/EntityLivingBase;FFF)V",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/util/EnumChatFormatting;getTextWithoutFormattingCodes(Ljava/lang/String;)Ljava/lang/String;"),
+            target = "Lnet/minecraft/util/EnumChatFormatting;getTextWithoutFormattingCodes(Ljava/lang/String;)Ljava/lang/String;",
+            remap = true),
         require = 0)
     private static String angelica$skipUnusedNameFormatting(String entityName) {
         return RendererLivingEntityHelper.stripFormattingCodes(entityName);
