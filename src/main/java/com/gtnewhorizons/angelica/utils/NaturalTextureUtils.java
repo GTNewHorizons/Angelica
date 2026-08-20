2f860c2af0c5e333b1bdeeef09180600c5d887dc
larvisen <arvid.junehag@gmail.com>
2026-08-20 21:03:30 +0200

    Got some feedback and made some changes oh yeah (volatile, change hash type)

--- src/main/java/com/gtnewhorizons/angelica/utils/NaturalTextureUtils.java
+++ src/main/java/com/gtnewhorizons/angelica/utils/NaturalTextureUtils.java
@@ -4,20 +4,22 @@ import com.gtnewhorizons.angelica.config.AngelicaConfig;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 
-import java.util.HashSet;
 import java.util.Set;
+
+import it.unimi.dsi.fastutil.objects.ReferenceOpenHashSet;
 
 public class NaturalTextureUtils {
 
-    private static Set<Block> cachedWhitelistedBlocks = null;
+    // Yes this is supposed to be volatile
+    private volatile static Set<Block> cachedWhitelistedBlocks = null;
 
-    // Get the top face orientation of the block with #math
     public static int getTopRotation(int x, int y, int z) {
         long hash = (x * 3129871L) ^ (z * 116586811L) ^ y;
         hash = hash * hash * 42317861L + hash * 11L;
-        return (int) ((hash >> 16) & 3);
+        return (int) (hash & 3);
     }
 
+    // This method gets called from worker threads
     public static boolean isNaturalBlock(Block block) {
         if (cachedWhitelistedBlocks == null) {
             // If list is empty add back all the stuff from config
@@ -32,7 +34,7 @@ public class NaturalTextureUtils {
     }
 
     private static void initCache() {
-        Set<Block> set = new HashSet<>();
+        Set<Block> set = new ReferenceOpenHashSet<>();
         if (AngelicaConfig.naturalTextureBlocks != null) {
             for (String name : AngelicaConfig.naturalTextureBlocks) {
                 Block b = (Block) Block.blockRegistry.getObject(name);
